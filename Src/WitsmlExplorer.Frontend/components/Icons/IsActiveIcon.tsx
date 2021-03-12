e4e7f29319f4b27519002a101941fc88aa0fd697
Hakon Hesselberg <hakon.hesselberg@gmail.com>
2021-03-12 11:56:09 +0100

    Add IsActiveIcon

---
+++ Src/WitsmlExplorer.Frontend/components/Icons/IsActiveIcon.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+
+export function IsActiveIcon(): React.ReactElement {
+  return (
+    <>
+      <img src="/assets/icons/trendingUp.svg" alt="Is Active" />
+    </>
+  );
+}
