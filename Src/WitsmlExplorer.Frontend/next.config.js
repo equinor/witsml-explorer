5188126b0b14ed819240e259fc4e8c2fb8a7c364
Aszalós Roland <roland.aszalos@tde-group.com>
2021-05-27 12:25:12 +0200

    pulled #423 changes into this branch

--- Src/WitsmlExplorer.Frontend/next.config.js
+++ Src/WitsmlExplorer.Frontend/next.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpack = require("webpack");
 const weURL = process.env.WITSMLEXPLORER_FRONTEND_URL;
-const wePath = weURL ? new URL(weURL).pathname : "";
+const wePath = weURL && weURL.length > 0 ? new URL(weURL).pathname : "";
 
 module.exports = {
   distDir: "build",
