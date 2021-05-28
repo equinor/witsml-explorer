293a19bd0e323a32ecd91f4dba1b61fa3e313d13
HAEGG <haegg@equinor.com>
2021-05-28 16:27:02 +0200

    Running tests

--- Src/WitsmlExplorer.Api/Models/Trajectory.cs
+++ Src/WitsmlExplorer.Api/Models/Trajectory.cs
@@ -7,18 +7,18 @@ namespace WitsmlExplorer.Api.Models
 {
     public class Trajectory
     {
-        public string Uid { get; internal set; }
+        public string Uid { get; set; }
         public decimal MdMin { get; internal set; }
         public decimal MdMax { get; internal set; }
         public string AziRef { get; internal set; }
         public DateTime? DTimTrajStart { get; internal set; }
         public DateTime? DTimTrajEnd { get; internal set; }
-        public string Name { get; internal set; }
+        public string Name { get; set; }
         public string NameWell { get; set; }
         public string NameWellbore { get; set; }
-        public string WellUid { get; internal set; }
+        public string WellUid { get; set; }
         public string WellboreUid { get; internal set; }
-        public List<TrajectoryStation> TrajectoryStations { get; internal set; }
+        public List<TrajectoryStation> TrajectoryStations { get; set; }
         public DateTime? DateTimeCreation { get; internal set; }
         public DateTime? DateTimeLastChange { get; internal set; }
         public CommonData CommonData { get; set; }
