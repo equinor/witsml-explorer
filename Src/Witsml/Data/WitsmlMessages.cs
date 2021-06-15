8bd1aaa99ebd815133b78f55756f9ae1809e209c
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2021-06-15 13:57:12 +0200

    Rename objects based on review feedback

--- Src/Witsml/Data/WitsmlMessages.cs
+++ Src/Witsml/Data/WitsmlMessages.cs
@@ -4,14 +4,14 @@ using System.Xml.Serialization;
 namespace Witsml.Data
 {
     [XmlRoot("messages", Namespace = "http://www.witsml.org/schemas/1series")]
-    public class WitsmlMessageObjects : IWitsmlQueryType
+    public class WitsmlMessages : IWitsmlQueryType
     {
 
         [XmlAttribute("version")]
         public string Version = "1.4.1.1";
 
         [XmlElement("message")]
-        public List<WitsmlMessageObject> MessageObjects { get; set; } = new List<WitsmlMessageObject>();
+        public List<WitsmlMessage> Messages { get; set; } = new List<WitsmlMessage>();
 
         public string TypeName => "message";
     }
