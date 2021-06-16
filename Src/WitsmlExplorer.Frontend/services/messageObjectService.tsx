1a12acf8a0b590017b61e10e6712964e7cf7dad9
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2021-06-16 12:58:49 +0200

    Add messages to API call

--- Src/WitsmlExplorer.Frontend/services/messageObjectService.tsx
+++ Src/WitsmlExplorer.Frontend/services/messageObjectService.tsx
@@ -2,12 +2,20 @@ import ApiClient from "./apiClient";
 import MessageObject, { emptyMessageObject } from "../models/messageObject";
 
 export default class MessageObjectService {
-  public static async getMessageObject(wellUid: string, wellboreUid: string, abortSignal?: AbortSignal): Promise<MessageObject> {
-    const response = await ApiClient.get(`/api/wells/${wellUid}/wellbores/${wellboreUid}`, abortSignal);
+  public static async getMessageObject(wellUid: string, wellboreUid: string, messageUid: string, abortSignal?: AbortSignal): Promise<MessageObject> {
+    const response = await ApiClient.get(`/api/wells/${wellUid}/wellbores/${wellboreUid}/messages/${messageUid}`, abortSignal);
     if (response.ok) {
       return response.json();
     } else {
       return emptyMessageObject();
+    }
+  }
+  public static async getMessageObjects(wellUid: string, wellboreUid: string, abortSignal?: AbortSignal): Promise<MessageObject[]> {
+    const response = await ApiClient.get(`/api/wells/${wellUid}/wellbores/${wellboreUid}/messages`, abortSignal);
+    if (response.ok) {
+      return response.json();
+    } else {
+      return [];
     }
   }
 }
