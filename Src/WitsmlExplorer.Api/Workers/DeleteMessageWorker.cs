3dce50aa1e77aec320a06cdcae72c138e8be6968
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2021-06-29 13:37:19 +0200

    Use MessageQueries

--- Src/WitsmlExplorer.Api/Workers/DeleteMessageWorker.cs
+++ Src/WitsmlExplorer.Api/Workers/DeleteMessageWorker.cs
@@ -24,38 +24,22 @@ namespace WitsmlExplorer.Api.Workers
         public async Task<(WorkerResult, RefreshAction)> Execute(DeleteMessageObjectJob job)
         {
             var targetWellbore = await GetWellbore(witsmlClient, job.MessageObject);
-            var deleteMessageQuery = DeleteMessageObjectQuery(job, targetWellbore);
+            var deleteMessageQuery = MessageQueries.GetMessageById(job.MessageObject.WellUid, job.MessageObject.WellboreUid, job.MessageObject.Uid);
             var deleteMessageResult = await witsmlClient.AddToStoreAsync(deleteMessageQuery);
 
             if (!deleteMessageResult.IsSuccessful)
             {
-                const string errorMessage = "Failed to create messageobject.";
-                Log.Error("{ErrorMessage}. Target: UidWell: {TargetWellUid}, UidWellbore: {TargetWellboreUid}.",
+                const string errorMessage = "Failed to delete messageobject.";
+                Log.Error("{ErrorMessage}. Target: UidWell: {TargetWellUid}, UidWellbore: {TargetWellboreUid}",
                     errorMessage, job.MessageObject.WellUid, job.MessageObject.WellboreUid);
                 return (new WorkerResult(witsmlClient.GetServerHostname(), false, errorMessage, deleteMessageResult.Reason), null);
             }
 
-            Log.Information("{JobType} - Job successful. MessageObject created", GetType().Name);
+            Log.Information("{JobType} - Job successful. MessageObject deleted", GetType().Name);
             var refreshAction = new RefreshWellbore(witsmlClient.GetServerHostname(), job.MessageObject.WellUid, job.MessageObject.WellboreUid, RefreshType.Update);
             var workerResult = new WorkerResult(witsmlClient.GetServerHostname(), true, $"MessageObject {job.MessageObject.Name} created for {targetWellbore.Name}");
 
             return (workerResult, refreshAction);
-        }
-
-        private static WitsmlMessages DeleteMessageObjectQuery(DeleteMessageObjectJob job, WitsmlWellbore targetWellbore)
-        {
-            return new()
-            {
-                Messages = new WitsmlMessage
-                {
-                    UidWell = targetWellbore.UidWell,
-                    NameWell = targetWellbore.NameWell,
-                    UidWellbore = targetWellbore.Uid,
-                    NameWellbore = targetWellbore.Name,
-                    Uid = job.MessageObject.Uid,
-                    Name = job.MessageObject.Name,
-                }.AsSingletonList()
-            };
         }
 
         private static async Task<WitsmlWellbore> GetWellbore(IWitsmlClient client, MessageObject messageObject)
