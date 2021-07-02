42e7ed83f0d724fe9eac5ff954ec132aafb22021
Stig Rune Søberg <ssob@equinor.com>
2021-07-02 11:20:05 +0200

    Added bhaRun to library and updated console app to be able to list them

---
+++ Src/Witsml/Data/WitsmlBhaRuns.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Witsml.Data
+{
+    [XmlRoot("bhaRuns", Namespace = "http://www.witsml.org/schemas/1series")]
+    public class WitsmlBhaRuns : IWitsmlQueryType
+    {
+        [XmlAttribute("version")] public string Version = "1.4.1.1";
+
+        [XmlElement("bhaRun")] public List<WitsmlBhaRun> BhaRuns { get; set; } = new List<WitsmlBhaRun>();
+        public string TypeName => "bhaRun";
+    }
+}
