a8f88138bf8f6a540640d844b88e14e00fc3a71e
Geir-Tore Lindsve <lgeir@equinor.com>
2021-11-03 14:31:54 +0100

    Added GetGrowingDataObjectFromStoreAsync for fetching Logs, MudLogs and Trajectories and returns resultCode in addition to deserialized data

--- Src/Witsml/Data/WitsmlMudLogs.cs
+++ Src/Witsml/Data/WitsmlMudLogs.cs
@@ -4,13 +4,13 @@ using System.Xml.Serialization;
 namespace Witsml.Data
 {
     [XmlRoot("mudLogs", Namespace = "http://www.witsml.org/schemas/1series")]
-    public class WitsmlMudLogs : IWitsmlQueryType
+    public class WitsmlMudLogs : IWitsmlGrowingDataQueryType
     {
         [XmlAttribute("version")]
         public string Version = "1.4.1.1";
 
         [XmlElement("mudLog")]
-        public List<WitsmlMudLog> MudLogs { get; set; } = new List<WitsmlMudLog>();
+        public List<WitsmlMudLog> MudLogs { get; set; } = new();
         public string TypeName => "mudLog";
     }
 }
