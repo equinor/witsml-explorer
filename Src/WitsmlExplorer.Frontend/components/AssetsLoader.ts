043deaa78de98861455334ce3ef847534ef990f1
Xiangyi Kong <XKong4@slb.com>
2021-12-07 17:53:03 +0800

    change asset prefix to the same as basePath

--- Src/WitsmlExplorer.Frontend/components/AssetsLoader.ts
+++ Src/WitsmlExplorer.Frontend/components/AssetsLoader.ts
@@ -1,6 +1,6 @@
 export class AssetsLoader {
   static getAssetsRoot(): string {
     const wePath = process.env.WITSMLEXPLORER_FRONTEND_URL;
-    return wePath && wePath.length > 0 ? wePath : "";
+    return wePath && wePath.length > 0 ? new URL(wePath).pathname : "";
   }
 }
