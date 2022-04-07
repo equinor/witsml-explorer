51e5a4de47abcdccb1f59543d44c04a82debb7c6
Xiangyi Kong <XKong4@slb.com>
2022-04-07 19:32:19 +0800

    upgrade carter to v6

--- Src/WitsmlExplorer.Api/Routes.cs
+++ Src/WitsmlExplorer.Api/Routes.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Carter;
-using Carter.Request;
 using Carter.Response;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using WitsmlExplorer.Api.Extensions;
 using WitsmlExplorer.Api.Models;
 using WitsmlExplorer.Api.Repositories;
@@ -14,7 +15,7 @@ using WitsmlExplorer.Api.Services;
 namespace WitsmlExplorer.Api
 {
     // ReSharper disable once UnusedMember.Global
-    public class Routes : CarterModule
+    public class Routes : ICarterModule
     {
         private readonly ICredentialsService credentialsService;
         private readonly IJobService jobService;
@@ -53,33 +54,40 @@ namespace WitsmlExplorer.Api
             this.mudLogService = mudLogService;
             this.witsmlServerRepository = witsmlServerRepository;
 
-            Get("/api/witsml-servers", GetWitsmlServers);
-            Post("/api/witsml-servers", CreateWitsmlServer);
-            Patch("/api/witsml-servers/{witsmlServerId}", UpdateWitsmlServer);
-            Delete("/api/witsml-servers/{witsmlServerId}", DeleteWitsmlServer);
 
-            Get("/api/wells", GetAllWells);
-            Get("/api/wells/{wellUid}", GetWell);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}", GetWellbore);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/messages", GetMessagesForWellbore);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/messages/{messageUid}", GetMessage);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}", GetLog);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs", GetLogsForWellbore);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}/logcurveinfo", GetLogCurveInfo);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}/logdata", GetLogData);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/rigs", GetRigsForWellbore);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/rigs/{rigUid}", GetRig);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/trajectories", GetTrajectories);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/trajectories/{trajectoryUid}/trajectorystations", GetTrajectoryStations);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/risks", GetRisksForWellbore);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/mudlogs", GetMudLogsForWellbore);
-            Get("/api/wells/{wellUid}/wellbores/{wellboreUid}/mudlogs/{mudlogUid}", GetMudLog);
+        }
+
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/witsml-servers", GetWitsmlServers);
+            app.MapPost("/api/witsml-servers", CreateWitsmlServer);
+            //MapPatch() extension methods will be available in .Net 7
+            app.MapMethods("/api/witsml-servers/{witsmlServerId}", new[] { HttpMethods.Patch }, UpdateWitsmlServer);
+            app.MapDelete("/api/witsml-servers/{witsmlServerId}", DeleteWitsmlServer);
+
+            app.MapGet("/api/wells", GetAllWells);
+            app.MapGet("/api/wells/{wellUid}", GetWell);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}", GetWellbore);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/messages", GetMessagesForWellbore);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/messages/{messageUid}", GetMessage);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}", GetLog);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs", GetLogsForWellbore);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}/logcurveinfo", GetLogCurveInfo);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}/logdata", GetLogData);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/rigs", GetRigsForWellbore);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/rigs/{rigUid}", GetRig);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/trajectories", GetTrajectories);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/trajectories/{trajectoryUid}/trajectorystations", GetTrajectoryStations);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/risks", GetRisksForWellbore);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/mudlogs", GetMudLogsForWellbore);
+            app.MapGet("/api/wells/{wellUid}/wellbores/{wellboreUid}/mudlogs/{mudlogUid}", GetMudLog);
 
             //Get Requests exceeding the URL limit
-            Post("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}/logdata", GetLargeLogData);
+            app.MapPost("/api/wells/{wellUid}/wellbores/{wellboreUid}/logs/{logUid}/logdata", GetLargeLogData);
 
-            Post("/api/jobs/{jobType}", CreateJob);
-            Post("/api/credentials/authorize", Authorize);
+            app.MapPost("/api/jobs/{jobType}", CreateJob);
+            app.MapPost("/api/credentials/authorize", Authorize);
         }
 
         private async Task GetWitsmlServers(HttpRequest httpRequest, HttpResponse httpResponse)
@@ -97,17 +105,15 @@ namespace WitsmlExplorer.Api
 
         private async Task UpdateWitsmlServer(HttpRequest httpRequest, HttpResponse httpResponse)
         {
-            var witsmlServerId = httpRequest.RouteValues.As<Guid>("witsmlServerId");
+            var witsmlServerId = httpRequest.RouteValues["witsmlServerId"] as Guid? ?? Guid.Empty;
             var patchedServer = await httpRequest.Body.Deserialize<Server>();
 
             var updatedServer = await witsmlServerRepository.UpdateDocumentAsync(witsmlServerId, patchedServer);
             await httpResponse.AsJson(updatedServer);
         }
 
-        private async Task DeleteWitsmlServer(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task DeleteWitsmlServer(HttpRequest httpRequest, HttpResponse httpResponse, Guid witsmlServerId)
         {
-            var witsmlServerId = httpRequest.RouteValues.As<Guid>("witsmlServerId");
-
             await witsmlServerRepository.DeleteDocumentAsync(witsmlServerId);
             httpResponse.StatusCode = StatusCodes.Status204NoContent;
         }
@@ -118,69 +124,50 @@ namespace WitsmlExplorer.Api
             await httpResponse.AsJson(allWells);
         }
 
-        private async Task GetWell(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetWell(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
             var well = await wellService.GetWell(wellUid);
             await httpResponse.AsJson(well);
         }
 
-        private async Task GetWellbore(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetWellbore(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var wellbore = await wellboreService.GetWellbore(wellUid, wellboreUid);
             await httpResponse.AsJson(wellbore);
         }
 
-        private async Task GetLogsForWellbore(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetLogsForWellbore(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var logs = await logObjectService.GetLogs(wellUid, wellboreUid);
             await httpResponse.AsJson(logs);
         }
 
-        private async Task GetLog(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetLog(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string logUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var logUid = httpRequest.RouteValues.As<string>("logUid");
             var log = await logObjectService.GetLog(wellUid, wellboreUid, logUid);
             await httpResponse.AsJson(log);
         }
 
-        private async Task GetMessagesForWellbore(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetMessagesForWellbore(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var messages = await messageObjectService.GetMessageObjects(wellUid, wellboreUid);
             await httpResponse.AsJson(messages);
         }
 
-        private async Task GetMessage(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetMessage(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string messageUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var messageUid = httpRequest.RouteValues.As<string>("messageUid");
             var message = await messageObjectService.GetMessageObject(wellUid, wellboreUid, messageUid);
             await httpResponse.AsJson(message);
         }
 
-        private async Task GetLogCurveInfo(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetLogCurveInfo(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string logUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var logUid = httpRequest.RouteValues.As<string>("logUid");
             var logCurveInfo = await logObjectService.GetLogCurveInfo(wellUid, wellboreUid, logUid);
             await httpResponse.AsJson(logCurveInfo);
         }
 
-        private async Task GetLogData(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetLogData(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string logUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var logUid = httpRequest.RouteValues.As<string>("logUid");
             if (httpRequest.Query.TryGetValue("mnemonic", out var mnemonics))
             {
                 string startIndex = null;
@@ -201,11 +188,8 @@ namespace WitsmlExplorer.Api
             }
         }
 
-        private async Task GetLargeLogData(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetLargeLogData(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string logUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var logUid = httpRequest.RouteValues.As<string>("logUid");
             var mnemonics = (await httpRequest.ReadFromJsonAsync<IEnumerable<string>>() ?? Array.Empty<string>()).ToList();
             if (mnemonics.Any())
             {
@@ -227,66 +211,48 @@ namespace WitsmlExplorer.Api
             }
         }
 
-        private async Task GetRigsForWellbore(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetRigsForWellbore(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var rigs = await rigService.GetRigs(wellUid, wellboreUid);
             await httpResponse.AsJson(rigs);
         }
 
-        private async Task GetRig(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetRig(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string rigUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var rigUid = httpRequest.RouteValues.As<string>("rigUid");
             var rig = await rigService.GetRig(wellUid, wellboreUid, rigUid);
             await httpResponse.AsJson(rig);
         }
 
-        private async Task GetTrajectories(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetTrajectories(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var trajectories = await trajectoryService.GetTrajectories(wellUid, wellboreUid);
             await httpResponse.AsJson(trajectories);
         }
 
-        private async Task GetTrajectoryStations(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetTrajectoryStations(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string trajectoryUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var trajectoryUid = httpRequest.RouteValues.As<string>("trajectoryUid");
             var trajectory = await trajectoryService.GetTrajectoryStations(wellUid, wellboreUid, trajectoryUid);
             await httpResponse.AsJson(trajectory);
         }
-        private async Task GetRisksForWellbore(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetRisksForWellbore(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var risks = await riskService.GetRisks(wellUid, wellboreUid);
             await httpResponse.AsJson(risks);
         }
 
-        private async Task GetMudLogsForWellbore(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetMudLogsForWellbore(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
             var mudLogs = await mudLogService.GetMudLogs(wellUid, wellboreUid);
             await httpResponse.AsJson(mudLogs);
         }
-        private async Task GetMudLog(HttpRequest httpRequest, HttpResponse httpResponse)
+        private async Task GetMudLog(HttpRequest httpRequest, HttpResponse httpResponse, string wellUid, string wellboreUid, string mudlogUid)
         {
-            var wellUid = httpRequest.RouteValues.As<string>("wellUid");
-            var wellboreUid = httpRequest.RouteValues.As<string>("wellboreUid");
-            var mudlogUid = httpRequest.RouteValues.As<string>("mudlogUid");
             var mudLog = await mudLogService.GetMudLog(wellUid, wellboreUid, mudlogUid);
             await httpResponse.AsJson(mudLog);
         }
 
-        private Task CreateJob(HttpRequest httpRequest, HttpResponse httpResponse)
+        private Task CreateJob(HttpRequest httpRequest, HttpResponse httpResponse, JobType jobType)
         {
-            var jobType = httpRequest.RouteValues.As<JobType>("jobType");
             jobService.CreateJob(jobType, httpRequest.Body);
             return Task.CompletedTask;
         }
@@ -297,5 +263,7 @@ namespace WitsmlExplorer.Api
             var result = await credentialsService.Authorize(witsmlServer.Url);
             await httpResponse.AsJson(result);
         }
+
+
     }
 }
