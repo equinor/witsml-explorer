1d837f76d31e5b741d065a50d6d9a92be34b02ce
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2022-04-26 14:19:12 +0200

    replace enzyme with react testing-library

--- Src/WitsmlExplorer.Frontend/setupTests.ts
+++ Src/WitsmlExplorer.Frontend/setupTests.ts
@@ -1,4 +1,1 @@
-import { configure } from "enzyme";
-import * as ReactSeventeenAdapter from "enzyme-adapter-react-17-updated";
-const adapter = ReactSeventeenAdapter as any;
-configure({ adapter: new adapter.default() });
+export {}
