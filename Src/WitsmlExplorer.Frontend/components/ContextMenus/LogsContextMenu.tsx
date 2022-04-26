aef5702a3fa6da4a0b77537fe4c4682d963ee473
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2022-04-26 16:14:59 +0200

    adjust for new version

--- Src/WitsmlExplorer.Frontend/components/ContextMenus/LogsContextMenu.tsx
+++ Src/WitsmlExplorer.Frontend/components/ContextMenus/LogsContextMenu.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Wellbore from "../../models/wellbore";
 import LogObject from "../../models/logObject";
-import { uuid } from "uuidv4";
+import { v4 as uuid } from "uuid";
 import LogPropertiesModal, { IndexCurve, LogPropertiesModalInterface } from "../Modals/LogPropertiesModal";
 import { DisplayModalAction, HideModalAction, HideContextMenuAction } from "../../contexts/operationStateReducer";
 import OperationType from "../../contexts/operationType";
