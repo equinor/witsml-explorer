8e3d6849108ef304e2a8271b4858b505d9621538
Eirin Svinsås <ESVI@equinor.com>
2022-06-29 14:35:59 +0200

    retrieving risk in wellboreService

--- Src/WitsmlExplorer.Frontend/services/wellboreService.tsx
+++ Src/WitsmlExplorer.Frontend/services/wellboreService.tsx
@@ -4,6 +4,7 @@ import LogObjectService from "./logObjectService";
 import TrajectoryService from "./trajectoryService";
 import RigService from "./rigService";
 import TubularService from "./tubularService";
+import RiskObjectService from "./riskObjectService";
 
 export default class WellboreService {
   public static async getWellbore(wellUid: string, wellboreUid: string, abortSignal?: AbortSignal): Promise<Wellbore> {
@@ -19,11 +20,12 @@ export default class WellboreService {
     const getWellbore = WellboreService.getWellbore(wellUid, wellboreUid);
     const getLogs = LogObjectService.getLogs(wellUid, wellboreUid);
     const getRigs = RigService.getRigs(wellUid, wellboreUid);
+    const getRisks = RiskObjectService.getRisks(wellUid, wellboreUid);
     //TODO find out whether "getMessages" should be called here
     const getTrajectories = TrajectoryService.getTrajectories(wellUid, wellboreUid);
     const getTubulars = TubularService.getTubulars(wellUid, wellboreUid);
-    const [wellbore, logs, rigs, trajectories, tubulars] = await Promise.all([getWellbore, getLogs, getRigs, getTrajectories, getTubulars]);
+    const [wellbore, logs, rigs, risks, trajectories, tubulars] = await Promise.all([getWellbore, getLogs, getRigs, getRisks, getTrajectories, getTubulars]);
 
-    return { ...wellbore, logs, rigs, trajectories, tubulars };
+    return { ...wellbore, logs, rigs, risks, trajectories, tubulars };
   }
 }
