3132b2ebeb7db3be317fda0c35341e693710e766
Andreas Stokkeland <ASTOKK@equinor.com>
2022-07-07 11:02:41 +0200

    Added bare minimum Witsml class for WellboreGeometry

---
+++ Src/Witsml/WitsmlWellboreGeometry.cs
@@ -0,0 +1,44 @@
+using System.Xml;
+using System.Xml.Serialization;
+using Witsml.Data.Measures;
+
+//Hvor kommer betydning av minOccurs inn?
+
+namespace Witsml.Data.WellboreGeometry
+{
+    public class WitsmlWellboreGeometry
+    {
+        [XmlAttribute("uidWell")]
+        public string UidWell { get; set; }
+
+        [XmlAttribute("uidWellbore")]
+        public string UidWellbore { get; set; }
+
+        [XmlAttribute("uid")]
+        public string Uid { get; set; }
+
+        [XmlElement("nameWell")]
+        public string NameWell { get; set; }
+
+        [XmlElement("nameWellbore")]
+        public string NameWellbore { get; set; }
+
+        [XmlElement("dTimReport")]
+        public string DTimReport { get; set; }
+
+        [XmlElement("mdBottom")]
+        public WitsmlIndex MdBottom { get; set; }
+
+        [XmlElement("gapAir")]
+        public Measure GapAir { get; set; }
+
+        [XmlElement("depthWaterMean")]
+        public Measure DepthWaterMean { get; set; }
+
+        [XmlElement("commonData")]
+        public WitsmlCommonData CommonData { get; set; }
+
+        [XmlElement("customData")]
+        public WitsmlCustomData CustomData { get; set; }
+    }
+}
