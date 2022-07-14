813a98ce2bc6a8aebd62c7a9f8d71518bd9905aa
Andreas Stokkeland <ASTOKK@equinor.com>
2022-07-14 09:20:00 +0200

    Removed extendCategory and sourceName from RiskReferences

--- Src/WitsmlExplorer.Api/Jobs/Common/RiskReferences.cs
+++ Src/WitsmlExplorer.Api/Jobs/Common/RiskReferences.cs
@@ -5,7 +5,5 @@ namespace WitsmlExplorer.Api.Jobs.Common
         public string WellUid { get; set; }
         public string WellboreUid { get; set; }
         public string[] RiskUids { get; set; }
-        public string ExtendCategory { get; set; }
-        public string SourceName { get; set; }
     }
 }
