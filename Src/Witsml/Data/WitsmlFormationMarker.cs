754a76a165e7666c7a71bb3a7c3fb2728abe76e2
Geir-Tore Lindsve <lgeir@equinor.com>
2022-08-01 12:05:24 +0200

    Added support for more fields from the Witml specification

--- Src/Witsml/Data/WitsmlFormationMarker.cs
+++ Src/Witsml/Data/WitsmlFormationMarker.cs
@@ -27,11 +27,38 @@ public class WitsmlFormationMarker
     [XmlElement("description")]
     public string Description { get; init; }
 
-    [XmlElement("tvdTopSample")]
-    public WitsmlWellVerticalDepthCoord TvdTopSample { get; init; }
+    [XmlElement("mdPrognosed")]
+    public WitsmlMeasuredDepthCoord MdPrognosed { get; init; }
+
+    [XmlElement("tvdPrognosed")]
+    public WitsmlWellVerticalDepthCoord TvdPrognosed { get; init; }
 
     [XmlElement("mdTopSample")]
     public WitsmlMeasuredDepthCoord MdTopSample { get; init; }
+
+    [XmlElement("tvdTopSample")]
+    public WitsmlWellVerticalDepthCoord TvdTopSample { get; init; }
+
+    [XmlElement("thicknessBed")]
+    public WitsmlLengthMeasure ThicknessBed { get; init; }
+
+    [XmlElement("thicknessApparent")]
+    public WitsmlLengthMeasure ThicknessApparent { get; init; }
+
+    [XmlElement("thicknessPerpen")]
+    public WitsmlLengthMeasure ThicknessPerpen { get; init; }
+
+    [XmlElement("mdLogSample")]
+    public WitsmlMeasuredDepthCoord MdLogSample { get; init; }
+
+    [XmlElement("tvdLogSample")]
+    public WitsmlWellVerticalDepthCoord TvdLogSample { get; init; }
+
+    [XmlElement("dip")]
+    public WitsmlPlaneAngleMeasure Dip { get; init; }
+
+    [XmlElement("dipDirection")]
+    public WitsmlPlaneAngleMeasure DipDirection { get; init; }
 
     [XmlElement("commonData")]
     public WitsmlCommonData CommonData { get; init; }
