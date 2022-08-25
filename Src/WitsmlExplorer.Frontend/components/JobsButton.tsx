207e98bead8b5dc3e7d5ec6b634ce049613ba2da
Jan Burak <jbura@equinor.com>
2022-08-25 15:20:38 +0100

    Disable jobs button on missing password

--- Src/WitsmlExplorer.Frontend/components/JobsButton.tsx
+++ Src/WitsmlExplorer.Frontend/components/JobsButton.tsx
@@ -2,6 +2,7 @@ import { Button } from "@material-ui/core";
 import React, { useContext } from "react";
 import NavigationContext from "../contexts/navigationContext";
 import NavigationType from "../contexts/navigationType";
+import CredentialsService from "../services/credentialsService";
 
 const JobsButton = (): React.ReactElement => {
   const { navigationState } = useContext(NavigationContext);
@@ -12,8 +13,10 @@ const JobsButton = (): React.ReactElement => {
     dispatchNavigation({ type: NavigationType.SelectJobs, payload: {} });
   };
 
+  const currentPassword = CredentialsService.getCredentials().find((cred) => cred.server.id == selectedServer.id)?.password;
+
   return (
-    <Button onClick={onClick} disabled={!selectedServer}>
+    <Button onClick={onClick} disabled={!selectedServer || !currentPassword}>
       Jobs
     </Button>
   );
