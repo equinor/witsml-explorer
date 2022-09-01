636be608fb72bfbffdf7cb8d617d791b6f72fdd7
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2022-09-01 11:45:19 +0200

    Switch to msal for test

--- Src/WitsmlExplorer.Frontend/components/Modals/ServerModal.tsx
+++ Src/WitsmlExplorer.Frontend/components/Modals/ServerModal.tsx
@@ -9,7 +9,7 @@ import { DisplayModalAction, HideModalAction } from "../../contexts/operationSta
 import OperationType from "../../contexts/operationType";
 import { Server } from "../../models/server";
 import { BasicServerCredentials } from "../../services/credentialsService";
-import ServerService from "../../services/serverService";
+import MsalServerService from "../../services/msalServerService";
 import { colors } from "../../styles/Colors";
 import ModalDialog from "./ModalDialog";
 import UserCredentialsModal, { CredentialsMode, UserCredentialsModalProps } from "./UserCredentialsModal";
@@ -37,10 +37,10 @@ const ServerModal = (props: ServerModalProps): React.ReactElement => {
 
     setIsLoading(true);
     if (isAddingNewServer) {
-      const freshServer = await ServerService.addServer(server, abortController.signal);
+      const freshServer = await MsalServerService.addServer(server, abortController.signal);
       dispatchNavigation({ type: ModificationType.AddServer, payload: { server: freshServer } });
     } else {
-      const freshServer = await ServerService.updateServer(server, abortController.signal);
+      const freshServer = await MsalServerService.updateServer(server, abortController.signal);
       dispatchNavigation({ type: ModificationType.UpdateServer, payload: { server: freshServer } });
     }
     setIsLoading(false);
@@ -81,7 +81,7 @@ const ServerModal = (props: ServerModalProps): React.ReactElement => {
       const abortController = new AbortController();
 
       try {
-        await ServerService.removeServer(server.id, abortController.signal);
+        await MsalServerService.removeServer(server.id, abortController.signal);
         dispatchNavigation({ type: ModificationType.RemoveServer, payload: { serverUid: server.id } });
       } catch (error) {
         //TODO Add a commmon way to handle such errors.
