4fa07b9a952fc567eecd699c086ab2dbff35b3d0
Jan Burak <jbura@equinor.com>
2022-09-20 08:56:40 +0100

    Fix menuItemText capitalization

--- Src/WitsmlExplorer.Frontend/components/ContextMenus/ContextMenuUtils.tsx
+++ Src/WitsmlExplorer.Frontend/components/ContextMenus/ContextMenuUtils.tsx
@@ -17,7 +17,7 @@ export const StyledIcon = styled(Icon)`
 export const menuItemText = (operation: string, object: string, array: any[] | null) => {
   const operationUpperCase = operation.charAt(0).toUpperCase() + operation.slice(1).toLowerCase();
   const objectLowercase = object.toLowerCase();
-  const objectPlural = objectLowercase.charAt(object.length - 1) == "y" ? objectLowercase.slice(0, object.length - 1) + "ies" : object + "s";
+  const objectPlural = objectLowercase.charAt(object.length - 1) == "y" ? objectLowercase.slice(0, object.length - 1) + "ies" : objectLowercase + "s";
   const isPlural = array ? array.length > 1 : false;
   const count = array?.length > 0 ? ` ${array.length} ` : " ";
   return `${operationUpperCase}${count}${isPlural ? objectPlural : objectLowercase}`;
--- Src/WitsmlExplorer.Frontend/components/ContextMenus/__tests__/ContextMenuUtils.test.ts
+++ Src/WitsmlExplorer.Frontend/components/ContextMenus/__tests__/ContextMenuUtils.test.ts
@@ -26,3 +26,7 @@ it("Should not show count when not given an array", () => {
 it("Should use defined capitalization", () => {
   expect(menuItemText("cOPY", "tRAJECTORY", null)).toStrictEqual("Copy trajectory");
 });
+
+it("Should use defined capitalization when plural", () => {
+  expect(menuItemText("cOPY", "bhaRun", ARRAY_LENGTH_2)).toStrictEqual("Copy 2 bharuns");
+});
