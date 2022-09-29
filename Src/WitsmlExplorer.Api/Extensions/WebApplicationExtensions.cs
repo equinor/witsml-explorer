8206dfb8d3ec0d5d7f845e3368f798c1810d71eb
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2022-09-29 13:54:05 +0200

    WE-570 revert to single endpoints for both securityschemes

--- Src/WitsmlExplorer.Api/Extensions/WebApplicationExtensions.cs
+++ Src/WitsmlExplorer.Api/Extensions/WebApplicationExtensions.cs
@@ -9,57 +9,74 @@ namespace WitsmlExplorer.Api.Extensions
 
     public static class WebApplicationExtensions
     {
-        private static readonly string OAuth2Path = "/secure";
-        private static readonly string BasicPath = "/api";
+        private static readonly string ApiPath = "/api";
         public static void MapGet(this IEndpointRouteBuilder endpoints, string pattern, Delegate handler, bool useOAuth2)
         {
             if (useOAuth2)
             {
-                endpoints.MapGet(OAuth2Path + pattern, handler).RequireAuthorization();
+                endpoints.MapGet(ApiPath + pattern, handler).RequireAuthorization();
             }
-            endpoints.MapGet(BasicPath + pattern, handler);
+            else
+            {
+                endpoints.MapGet(ApiPath + pattern, handler);
+            }
         }
 
         public static void MapGet(this IEndpointRouteBuilder endpoints, string pattern, Delegate handler, bool useOAuth2, params string[] policyNames)
         {
             if (useOAuth2)
             {
-                endpoints.MapGet(OAuth2Path + pattern, handler).RequireAuthorization(policyNames);
+                endpoints.MapGet(ApiPath + pattern, handler).RequireAuthorization(policyNames);
             }
-            endpoints.MapGet(BasicPath + pattern, handler);
+            else
+            {
+                endpoints.MapGet(ApiPath + pattern, handler);
+            }
         }
         public static void MapPost(this IEndpointRouteBuilder endpoints, string pattern, Delegate handler, bool useOAuth2)
         {
             if (useOAuth2)
             {
-                endpoints.MapPost(OAuth2Path + pattern, handler).RequireAuthorization();
+                endpoints.MapPost(ApiPath + pattern, handler).RequireAuthorization();
             }
-            endpoints.MapPost(BasicPath + pattern, handler);
+            else
+            {
+                endpoints.MapPost(ApiPath + pattern, handler);
+            }
         }
         public static void MapPost(this IEndpointRouteBuilder endpoints, string pattern, Delegate handler, bool useOAuth2, params string[] policyNames)
         {
             if (useOAuth2)
             {
-                endpoints.MapPost(OAuth2Path + pattern, handler).RequireAuthorization(policyNames);
+                endpoints.MapPost(ApiPath + pattern, handler).RequireAuthorization(policyNames);
             }
-            endpoints.MapPost(BasicPath + pattern, handler);
+            else
+            {
+                endpoints.MapPost(ApiPath + pattern, handler);
+            }
         }
         public static void MapDelete(this IEndpointRouteBuilder endpoints, string pattern, Delegate handler, bool useOAuth2, params string[] policyNames)
         {
             if (useOAuth2)
             {
-                endpoints.MapDelete(OAuth2Path + pattern, handler).RequireAuthorization(policyNames);
+                endpoints.MapDelete(ApiPath + pattern, handler).RequireAuthorization(policyNames);
             }
-            endpoints.MapDelete(BasicPath + pattern, handler);
+            else
+            {
+                endpoints.MapDelete(ApiPath + pattern, handler);
+            }
         }
 
         public static void MapMethods(this IEndpointRouteBuilder endpoints, string pattern, IEnumerable<string> httpMethods, Delegate handler, bool useOAuth2, params string[] policyNames)
         {
             if (useOAuth2)
             {
-                endpoints.MapMethods(OAuth2Path + pattern, httpMethods, handler).RequireAuthorization(policyNames);
+                endpoints.MapMethods(ApiPath + pattern, httpMethods, handler).RequireAuthorization(policyNames);
             }
-            endpoints.MapMethods(BasicPath + pattern, httpMethods, handler);
+            else
+            {
+                endpoints.MapMethods(ApiPath + pattern, httpMethods, handler);
+            }
         }
     }
 }
