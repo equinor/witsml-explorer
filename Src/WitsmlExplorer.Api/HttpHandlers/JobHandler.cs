e7b4106dd0b523c39031c3ecd70deb4d86443e96
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2022-10-19 11:58:42 +0200

    WE-592 adjust JobHandler to distinguish between Basic and OAuth ID in jobinfo

--- Src/WitsmlExplorer.Api/HttpHandlers/JobHandler.cs
+++ Src/WitsmlExplorer.Api/HttpHandlers/JobHandler.cs
@@ -16,18 +16,19 @@ namespace WitsmlExplorer.Api.HttpHandlers
     public static class JobHandler
     {
         [Produces(typeof(string))]
-        public static async Task<IResult> CreateJob(JobType jobType, HttpRequest httpRequest, IJobService jobService, ICredentialsService credentialsService)
+        public static async Task<IResult> CreateJob(JobType jobType, HttpRequest httpRequest, IConfiguration configuration, IJobService jobService, ICredentialsService credentialsService)
         {
 
             string bearerAuth = httpRequest.Headers["Authorization"];
             string basicAuth = httpRequest.Headers[WitsmlClientProvider.WitsmlTargetServerHeader];
             ServerCredentials witsmlTarget = httpRequest.GetWitsmlServerHttpHeader(WitsmlClientProvider.WitsmlTargetServerHeader, n => "");
             ServerCredentials witsmlSource = httpRequest.GetWitsmlServerHttpHeader(WitsmlClientProvider.WitsmlSourceServerHeader, n => "");
+            bool useOAuth2 = StringHelpers.ToBoolean(configuration[ConfigConstants.OAuth2Enabled]);
 
-            (string username, string witsmlUsername) = credentialsService.GetUsernamesFromHeaderValues(bearerAuth, basicAuth);
+            (string userPrincipalName, string witsmlUsername) = credentialsService.GetUsernamesFromHeaderValues(bearerAuth, basicAuth);
             JobInfo jobInfo = new()
             {
-                Username = username,
+                Username = useOAuth2 ? userPrincipalName : witsmlUsername,
                 WitsmlUsername = witsmlUsername,
                 SourceServer = witsmlSource.Host?.ToString(),
                 TargetServer = witsmlTarget.Host?.ToString()
@@ -38,15 +39,18 @@ namespace WitsmlExplorer.Api.HttpHandlers
         [Produces(typeof(IEnumerable<JobInfo>))]
         public static IResult GetJobInfosByAuthorizedUser(IJobCache jobCache, HttpRequest httpRequest, IConfiguration configuration, ICredentialsService credentialsService)
         {
+            bool useOAuth2 = StringHelpers.ToBoolean(configuration[ConfigConstants.OAuth2Enabled]);
             string bearerAuth = httpRequest.Headers["Authorization"];
             string basicAuth = httpRequest.Headers[WitsmlClientProvider.WitsmlTargetServerHeader];
-            (string userPrincipalName, _) = credentialsService.GetUsernamesFromHeaderValues(bearerAuth, basicAuth);
-            bool useOAuth2 = StringHelpers.ToBoolean(configuration[ConfigConstants.OAuth2Enabled]);
-            if (!useOAuth2 && !credentialsService.ValidEncryptedBasicCredentials(basicAuth))
+            (string userPrincipalName, string witsmlUserName) = credentialsService.GetUsernamesFromHeaderValues(bearerAuth, basicAuth);
+
+            if ((useOAuth2 && string.IsNullOrEmpty(userPrincipalName)) ||
+            (!useOAuth2 && string.IsNullOrEmpty(witsmlUserName)))
             {
                 return Results.Unauthorized();
             }
-            return Results.Ok(jobCache.GetJobInfosByUser(userPrincipalName));
+
+            return useOAuth2 ? Results.Ok(jobCache.GetJobInfosByUser(userPrincipalName)) : Results.Ok(jobCache.GetJobInfosByUser(witsmlUserName));
         }
 
     }
