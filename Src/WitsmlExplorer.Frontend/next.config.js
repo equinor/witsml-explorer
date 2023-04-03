47eb5dc97763868c20d20acca9dab598c37286df
Xiangyi Kong <XKong4@slb.com>
2023-04-03 17:21:37 +0800

    remove useless eslint rules

--- Src/WitsmlExplorer.Frontend/next.config.js
+++ Src/WitsmlExplorer.Frontend/next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const weURL = process.env.NEXT_PUBLIC_WITSMLEXPLORER_FRONTEND_URL;
 const wePath = weURL && weURL.length > 0 ? new URL(weURL).pathname : "";
 
