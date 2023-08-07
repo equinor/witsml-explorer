1f5e03754d791699288e153d157edab2787c29a6
Geir-Tore Lindsve <lgeir@equinor.com>
2023-08-07 15:02:19 +0200

    Updated WitsmlClient constructor to use lambda syntax for options (inspired by ASP.NET)

--- Src/Witsml/WitsmlClient.cs
+++ Src/Witsml/WitsmlClient.cs
@@ -36,28 +36,31 @@ namespace Witsml
         [Obsolete("Use the WitsmlClientOptions based constructor instead")]
         public WitsmlClient(string hostname, string username, string password, WitsmlClientCapabilities clientCapabilities, TimeSpan? requestTimeout = null,
             bool logQueries = false)
-            : this(new WitsmlClientOptions
+            : this(options =>
             {
-                Hostname = hostname,
-                Credentials = new WitsmlCredentials(username, password),
-                ClientCapabilities = clientCapabilities,
-                RequestTimeOut = requestTimeout ?? TimeSpan.FromMinutes(1),
-                LogQueries = logQueries
+                options.Hostname = hostname;
+                options.Credentials = new WitsmlCredentials(username, password);
+                options.ClientCapabilities = clientCapabilities;
+                options.RequestTimeOut = requestTimeout ?? TimeSpan.FromMinutes(1);
+                options.LogQueries = logQueries;
             })
         { }
 
-        public WitsmlClient(WitsmlClientOptions options)
+        public WitsmlClient(Action<WitsmlClientOptions> options)
         {
-            ArgumentNullException.ThrowIfNull(options.Hostname);
-            ArgumentNullException.ThrowIfNull(options.Credentials.Username);
-            ArgumentNullException.ThrowIfNull(options.Credentials.Password);
+            var witsmlClientOptions = new WitsmlClientOptions();
+            options(witsmlClientOptions);
 
-            _clientCapabilities = options.ClientCapabilities.ToXml();
-            _serverUrl = new Uri(options.Hostname);
+            ArgumentNullException.ThrowIfNull(witsmlClientOptions.Hostname);
+            ArgumentNullException.ThrowIfNull(witsmlClientOptions.Credentials.Username);
+            ArgumentNullException.ThrowIfNull(witsmlClientOptions.Credentials.Password);
 
-            _client = CreateSoapClient(options);
+            _clientCapabilities = witsmlClientOptions.ClientCapabilities.ToXml();
+            _serverUrl = new Uri(witsmlClientOptions.Hostname);
 
-            SetupQueryLogging(options.LogQueries);
+            _client = CreateSoapClient(witsmlClientOptions);
+
+            SetupQueryLogging(witsmlClientOptions.LogQueries);
         }
 
         private void SetupQueryLogging(bool logQueries)
