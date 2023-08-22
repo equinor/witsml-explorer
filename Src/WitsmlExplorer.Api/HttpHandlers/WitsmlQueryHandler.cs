6c7e6d7852a312df23845305bc5a0d1319eac488
Jan Burak <jan@kodeworks.no>
2023-08-22 09:46:41 +0100

    FIX-1951 show login modal on missing credentials

--- Src/WitsmlExplorer.Api/HttpHandlers/WitsmlQueryHandler.cs
+++ Src/WitsmlExplorer.Api/HttpHandlers/WitsmlQueryHandler.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using Witsml;
 using Witsml.ServiceReference;
 
 using WitsmlExplorer.Api.Extensions;
+using WitsmlExplorer.Api.Middleware;
 using WitsmlExplorer.Api.Models;
 using WitsmlExplorer.Api.Services;
 
@@ -18,11 +21,12 @@ namespace WitsmlExplorer.Api.HttpHandlers
         [Produces(typeof(string))]
         public static async Task<IResult> PostQuery(IWitsmlClientProvider witsmlClientProvider, HttpRequest httpRequest)
         {
+            IWitsmlClient witsmlClient = witsmlClientProvider.GetClient() ?? throw new WitsmlClientProviderException($"No WITSML access", (int)HttpStatusCode.Unauthorized, ServerType.Target);
             try
             {
                 WitsmlQuery query = await httpRequest.Body.Deserialize<WitsmlQuery>();
                 // file deepcode ignore XmlInjection: the body is only used to retrieve the query type that is sent further to the WITSML server
-                string result = await witsmlClientProvider.GetClient().GetFromStoreAsync(query.Body, new OptionsIn(query.ReturnElements));
+                string result = await witsmlClient.GetFromStoreAsync(query.Body, new OptionsIn(query.ReturnElements));
                 return TypedResults.Ok(result);
             }
             catch (Exception e)
