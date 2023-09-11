82f842abf9d9665d971644c2fa2e0b808599f63b
shijo <shijo.rabi@shellsquare.com>
2023-09-11 16:36:48 +0530

    eslint issue FIx #1863

--- Src/WitsmlExplorer.Frontend/components/Modals/TrajectoryPropertiesModal.tsx
+++ Src/WitsmlExplorer.Frontend/components/Modals/TrajectoryPropertiesModal.tsx
@@ -1,4 +1,4 @@
-﻿﻿import { TextField } from "@material-ui/core";
+﻿import { TextField } from "@material-ui/core";
 import React, { useContext, useEffect, useState } from "react";
 import { HideModalAction } from "../../contexts/operationStateReducer";
 import OperationType from "../../contexts/operationType";
