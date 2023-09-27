90e66da40485632c4b1da3f3b9374da65ffe7846
shijo <shijo.rabi@shellsquare.com>
2023-09-27 17:44:57 +0530

    resolve conflict PR: #2047

--- Src/WitsmlExplorer.Frontend/components/Constants.tsx
+++ Src/WitsmlExplorer.Frontend/components/Constants.tsx
@@ -13,3 +13,4 @@ export const STORAGE_TIMEZONE_KEY = "selectedTimeZone";
 export const STORAGE_MODE_KEY = "selectedMode";
 export const STORAGE_FILTER_HIDDENOBJECTS_KEY = "hiddenObjects";
 export const STORAGE_MISSING_DATA_AGENT_CHECKS_KEY = "missingDataAgentChecks";
+export const STORAGE_DATETIMEFORMAT_KEY = "selectedDateTimeFormat";
