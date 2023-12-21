10641360fcf077a4ab4ef4bd39ee53ccf2c7760d
Jan-Marius Vatle <jvat@equinor.com>
2023-12-21 17:53:43 +0100

    FIX-2114 added protected routes

---
+++ Src/WitsmlExplorer.Frontend/routes/AuthRoute.tsx
@@ -0,0 +1,195 @@
+import { useIsAuthenticated } from "@azure/msal-react";
+import { useContext, useEffect, useState } from "react";
+import { Outlet, useParams } from "react-router-dom";
+import UserCredentialsModal, {
+  UserCredentialsModalProps
+} from "../components/Modals/UserCredentialsModal";
+import {
+  FilterContext,
+  VisibilityStatus,
+  allVisibleObjects
+} from "../contexts/filter";
+import { UpdateServerListAction } from "../contexts/modificationActions";
+import ModificationType from "../contexts/modificationType";
+import { SelectServerAction } from "../contexts/navigationActions";
+import NavigationContext from "../contexts/navigationContext";
+import NavigationType from "../contexts/navigationType";
+import OperationContext from "../contexts/operationContext";
+import OperationType from "../contexts/operationType";
+import { ObjectType } from "../models/objectType";
+import { Server } from "../models/server";
+import { msalEnabled } from "../msal/MsalAuthProvider";
+import AuthorizationService, {
+  AuthorizationState,
+  AuthorizationStatus
+} from "../services/authorizationService";
+import CapService from "../services/capService";
+import NotificationService from "../services/notificationService";
+import ServerService from "../services/serverService";
+import WellService from "../services/wellService";
+import {
+  STORAGE_FILTER_HIDDENOBJECTS_KEY,
+  getLocalStorageItem
+} from "../tools/localStorageHelpers";
+
+export default function AuthRoute() {
+  const { dispatchNavigation, navigationState } = useContext(NavigationContext);
+  const { updateSelectedFilter } = useContext(FilterContext);
+  const { servers, selectedServer, wells } = navigationState;
+  const [hasFetchedServers, setHasFetchedServers] = useState(false);
+  const isAuthenticated = !msalEnabled || useIsAuthenticated();
+  const { serverUrl } = useParams();
+  const { dispatchOperation } = useContext(OperationContext);
+  const [authorizationState, setAuthorizationState] =
+    useState<AuthorizationState>();
+
+  useEffect(() => {
+    const unsubscribeFromCredentialsEvents =
+      AuthorizationService.onAuthorizationChangeEvent.subscribe(
+        async (authorizationState) => {
+          setAuthorizationState(authorizationState);
+        }
+      );
+    return () => {
+      unsubscribeFromCredentialsEvents();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isAuthenticated && !hasFetchedServers) {
+      const abortController = new AbortController();
+      const getServers = async () => {
+        const freshServers = await ServerService.getServers(
+          abortController.signal
+        );
+        setHasFetchedServers(true);
+        const action: UpdateServerListAction = {
+          type: ModificationType.UpdateServerList,
+          payload: { servers: freshServers }
+        };
+        dispatchNavigation(action);
+      };
+      getServers();
+
+      return () => {
+        abortController.abort();
+      };
+    }
+  }, [isAuthenticated, hasFetchedServers]);
+
+  useEffect(() => {
+    // update selected server when servers are fetched
+    if (servers.length !== 0) {
+      if (serverUrl == null) {
+        return;
+      }
+      const server = servers.find(
+        (server: Server) => server.url.toLowerCase() === serverUrl.toLowerCase()
+      );
+      if (server && !selectedServer) {
+        const onSelectItem = async (server: Server) => {
+          if (server.id === selectedServer?.id) {
+            const action: SelectServerAction = {
+              type: NavigationType.SelectServer,
+              payload: { server: null }
+            };
+            dispatchNavigation(action);
+          } else {
+            const userCredentialsModalProps: UserCredentialsModalProps = {
+              server,
+              onConnectionVerified: (username) => {
+                dispatchOperation({ type: OperationType.HideModal });
+                AuthorizationService.onAuthorized(
+                  server,
+                  username,
+                  dispatchNavigation
+                );
+                const action: SelectServerAction = {
+                  type: NavigationType.SelectServer,
+                  payload: { server }
+                };
+                dispatchNavigation(action);
+              }
+            };
+            dispatchOperation({
+              type: OperationType.DisplayModal,
+              payload: <UserCredentialsModal {...userCredentialsModalProps} />
+            });
+          }
+        };
+
+        onSelectItem(server);
+      }
+    }
+  }, [servers]);
+
+  const updateVisibleObjects = (supportedObjects: string[]) => {
+    const updatedVisibility = { ...allVisibleObjects };
+    const hiddenItems = getLocalStorageItem<ObjectType[]>(
+      STORAGE_FILTER_HIDDENOBJECTS_KEY,
+      { defaultValue: [] }
+    );
+    hiddenItems.forEach(
+      (objectType) => (updatedVisibility[objectType] = VisibilityStatus.Hidden)
+    );
+    Object.values(ObjectType)
+      .filter(
+        (objectType) =>
+          !supportedObjects
+            .map((o) => o.toLowerCase())
+            .includes(objectType.toLowerCase())
+      )
+      .forEach(
+        (objectType) =>
+          (updatedVisibility[objectType] = VisibilityStatus.Disabled)
+      );
+    updateSelectedFilter({ objectVisibilityStatus: updatedVisibility });
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const onCurrentLoginStateChange = async () => {
+      if (
+        selectedServer == null ||
+        wells.length !== 0 ||
+        (authorizationState &&
+          authorizationState.status != AuthorizationStatus.Authorized)
+      ) {
+        return;
+      }
+      try {
+        const [wells, supportedObjects] = await Promise.all([
+          WellService.getWells(),
+          CapService.getCapObjects()
+        ]);
+        updateVisibleObjects(supportedObjects);
+        dispatchNavigation({
+          type: ModificationType.UpdateWells,
+          payload: { wells: wells }
+        });
+      } catch (error) {
+        NotificationService.Instance.alertDispatcher.dispatch({
+          serverUrl: new URL(selectedServer.url),
+          message: error.message,
+          isSuccess: false
+        });
+        dispatchNavigation({
+          type: NavigationType.SelectServer,
+          payload: { server: null }
+        });
+      }
+    };
+    onCurrentLoginStateChange();
+    return () => {
+      abortController.abort();
+    };
+  }, [msalEnabled, selectedServer, authorizationState]);
+
+  if (
+    authorizationState &&
+    authorizationState.status === AuthorizationStatus.Authorized
+  ) {
+    return <Outlet />;
+  }
+  return <></>;
+}
--- Src/WitsmlExplorer.Frontend/routes/Router.tsx
+++ Src/WitsmlExplorer.Frontend/routes/Router.tsx
@@ -12,6 +12,7 @@ import ServerManager from "../components/ContentViews/ServerManager";
 import WellboreObjectTypesListView from "../components/ContentViews/WellboreObjectTypesListView";
 import WellboresListView from "../components/ContentViews/WellboresListView";
 import WellsListView from "../components/ContentViews/WellsListView";
+import AuthRoute from "./AuthRoute";
 import Root from "./Root";
 
 const router = createBrowserRouter([
@@ -20,57 +21,62 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { index: true, element: <ServerManager /> },
-
-      { path: "servers/:serverUrl/wells", element: <WellsListView /> },
       {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores",
-        element: <WellboresListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid",
-        element: <WellboreObjectTypesListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/bharuns",
-        element: <BhaRunsListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/changelogs",
-        element: <ChangeLogsListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/fluidsreports",
-        element: <FluidsReportsListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/formationmarkers",
-        element: <FormationMarkersListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/logs",
-        element: <LogTypeListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/logs/depth",
-        element: <LogsListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/logs/time",
-        element: <LogsListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/logs/depth/:logUid",
-        element: <LogCurveInfoListView />
-      },
-      {
-        path: "servers/:serverUrl/wells/:wellUid/wellbores/:wellboreUid/logs/time/:logUid",
-        element: <LogCurveInfoListView />
-      },
-      {
-        path: "servers/:serverUrl/jobs",
-        element: <JobsView />
-      },
-      { path: "servers/:serverUrl/query", element: <QueryView /> }
+        path: "servers/:serverUrl",
+        element: <AuthRoute />,
+        children: [
+          { path: "wells", element: <WellsListView /> },
+          {
+            path: "wells/:wellUid/wellbores",
+            element: <WellboresListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid",
+            element: <WellboreObjectTypesListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/bharuns",
+            element: <BhaRunsListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/changelogs",
+            element: <ChangeLogsListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/fluidsreports",
+            element: <FluidsReportsListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/formationmarkers",
+            element: <FormationMarkersListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/logs",
+            element: <LogTypeListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/logs/depth",
+            element: <LogsListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/logs/time",
+            element: <LogsListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/logs/depth/:logUid",
+            element: <LogCurveInfoListView />
+          },
+          {
+            path: "wells/:wellUid/wellbores/:wellboreUid/logs/time/:logUid",
+            element: <LogCurveInfoListView />
+          },
+          {
+            path: "jobs",
+            element: <JobsView />
+          },
+          { path: "query", element: <QueryView /> }
+        ]
+      }
     ]
   }
 ]);
