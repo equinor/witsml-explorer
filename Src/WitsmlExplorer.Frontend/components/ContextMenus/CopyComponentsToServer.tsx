67a8d78066c21242b5a761f5f7ed0dcc92939496
Robert Básti <robert.basti@tietoevry.com>
2024-01-08 17:25:35 +0100

    Copy with range to server #2051 - PR

--- Src/WitsmlExplorer.Frontend/components/ContextMenus/CopyComponentsToServer.tsx
+++ Src/WitsmlExplorer.Frontend/components/ContextMenus/CopyComponentsToServer.tsx
@@ -1,35 +1,17 @@
 import { Typography } from "@equinor/eds-core-react";
 import { MenuItem } from "@material-ui/core";
-import {
-  menuItemText,
-  pluralize
-} from "components/ContextMenus/ContextMenuUtils";
+import { menuItemText } from "components/ContextMenus/ContextMenuUtils";
 import { useContext } from "react";
 import NestedMenuItem from "components/ContextMenus/NestedMenuItem";
-import { displayMissingObjectModal } from "components/Modals/MissingObjectModals";
-import { displayReplaceModal } from "components/Modals/ReplaceModal";
 import NavigationContext from "contexts/navigationContext";
 import OperationContext from "contexts/operationContext";
-import { DispatchOperation } from "contexts/operationStateReducer";
 import OperationType from "contexts/operationType";
-import { ComponentType, getParentType } from "models/componentType";
-import ComponentReferences, {
-  createComponentReferences
-} from "models/jobs/componentReferences";
-import { CopyComponentsJob } from "models/jobs/copyJobs";
-import { DeleteComponentsJob } from "models/jobs/deleteJobs";
-import ObjectReference from "models/jobs/objectReference";
-import { ReplaceComponentsJob } from "models/jobs/replaceComponentsJob";
-import LogCurveInfo from "models/logCurveInfo";
-import ObjectOnWellbore, { toObjectReference } from "models/objectOnWellbore";
+import { ComponentType } from "models/componentType";
 import { Server } from "models/server";
-import { Fragment, useContext } from "react";
-import AuthorizationService from "services/authorizationService";
-import ComponentService from "services/componentService";
-import JobService, { JobType } from "services/jobService";
-import ObjectService from "services/objectService";
 import { copyComponentsToServer } from "./CopyComponentsToServerUtils";
-import CopyRangeModal, { CopyRangeModalProps } from "components/Modals/CopyRangeModal";
+import CopyRangeModal, {
+  CopyRangeModalProps
+} from "components/Modals/CopyRangeModal";
 
 export interface CopyComponentsToServerMenuItemProps {
   componentsToCopy: { uid: string }[];
