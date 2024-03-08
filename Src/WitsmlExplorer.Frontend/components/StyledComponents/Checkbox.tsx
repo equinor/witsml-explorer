f1a289650b26a631a6de510458b1927d74b7fba8
Jan-Marius Vatle (Omega AS) <JVAT@equinor.com>
2024-03-08 15:21:28 +0100

    FIX-2286 fix disabled checkbox styling

--- Src/WitsmlExplorer.Frontend/components/StyledComponents/Checkbox.tsx
+++ Src/WitsmlExplorer.Frontend/components/StyledComponents/Checkbox.tsx
@@ -2,8 +2,20 @@ import { Checkbox as EdsCheckbox } from "@equinor/eds-core-react";
 import styled from "styled-components";
 import { Colors } from "styles/Colors";
 
-export const Checkbox = styled(EdsCheckbox)<{ colors: Colors }>`
+export const Checkbox = styled(EdsCheckbox)<{
+  colors: Colors;
+  disabled: boolean;
+}>`
   span {
-    color: ${(props) => props.colors.text.staticIconsDefault};
+    color: ${(props) =>
+      props.disabled
+        ? props.colors.text.disabledText
+        : props.colors.infographic.primaryMossGreen};
+  }
+  input[type="checkbox"] + svg {
+    fill: ${(props) =>
+      props.disabled
+        ? props.colors.text.disabledText
+        : props.colors.infographic.primaryMossGreen};
   }
 `;
