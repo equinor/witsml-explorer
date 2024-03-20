72a22d63ece98d17e316e9924c5c8cd71aedb4c2
Jan-Marius Vatle (Omega AS) <JVAT@equinor.com>
2024-03-20 10:27:34 +0100

    FIX-2323 remove the last bits of the NEXT_PUBLIC_WITSMLEXPLORER_FRONTEND_URL

--- Src/WitsmlExplorer.Frontend/next.config.js
+++ Src/WitsmlExplorer.Frontend/next.config.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-undef */
-const weURL = process.env.NEXT_PUBLIC_WITSMLEXPLORER_FRONTEND_URL;
-const wePath = weURL && weURL.length > 0 ? new URL(weURL).pathname : "";
 
 module.exports = {
   distDir: "build",
-  basePath: wePath,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.xml$/i,
