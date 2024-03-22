300025c8a50a050ecc115015c34a2c2802262e45
Václav Bašniar <vaclav.basniar@tietoevry.com>
2024-03-22 10:47:26 +0100

    removed JobType Json serialization

--- Src/WitsmlExplorer.Api/Models/JobType.cs
+++ Src/WitsmlExplorer.Api/Models/JobType.cs
@@ -1,8 +1,5 @@
-using System.Text.Json.Serialization;
-
 namespace WitsmlExplorer.Api.Models
 {
-    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum JobType
     {
         CopyComponents = 1,
