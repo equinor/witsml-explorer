ed967e3219d16e3b1ba0c90caa355a59bf5de448
Jan-Marius Vatle (Omega AS) <JVAT@equinor.com>
2024-04-02 15:02:40 +0200

    vitest remove observer

--- Src/WitsmlExplorer.Frontend/components/Modals/__tests__/ReportModal.test.tsx
+++ Src/WitsmlExplorer.Frontend/components/Modals/__tests__/ReportModal.test.tsx
@@ -1,7 +1,6 @@
 import { act, screen, within } from "@testing-library/react";
 import { mockEdsCoreReact } from "__testUtils__/mocks/EDSMocks";
 import {
-  MockResizeObserver,
   deferred,
   getJobInfo,
   getNotification,
@@ -25,9 +24,6 @@ vi.mock("services/jobService", () => {
 });
 
 describe("Report Modal", () => {
-  //mock ResizeObserver to enable testing virtualized components
-  window.ResizeObserver = MockResizeObserver;
-
   describe("Report Modal with report", () => {
     it("Should show a basic report", () => {
       renderWithContexts(<ReportModal report={REPORT} />);
