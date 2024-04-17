9301e36d3558f34b64b65e97e949ca32bdbd7102
Jan-Marius Vatle (Omega AS) <JVAT@equinor.com>
2024-04-17 13:04:06 +0200

    wait for API and set window size

--- Electron/src/main/main.js
+++ Electron/src/main/main.js
@@ -1,11 +1,22 @@
 /* eslint-env node */
 
 import { spawn } from 'cross-spawn';
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import * as path from 'path';
 
 let mainWindow;
 let apiProcess;
+
+// Function to manual control a Promise
+function deferred() {
+    let resolve;
+    let reject;
+    const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+    });
+    return { promise, resolve, reject };
+}
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -17,8 +28,8 @@ function getProductionPath(relativePath, isAsarUnpacked = false) {
         return path.join(__dirname, '../', relativePath);
     }
 }
+async function startApi() {
 
-function startApi() {
     if (isDevelopment) {
         const basePath = app.getAppPath();
         const env = {
@@ -35,15 +46,39 @@ function startApi() {
         apiProcess = spawn(path.join(apiPath, 'WitsmlExplorer.Api'), [], { env, cwd: apiPath });
     }
 
+    // Promise that is manual resolved when API has started.
+    const { promise, resolve, reject } = deferred();
+
+    // The app will wait 15 seconds for the API to start, if not it will be forced quit.
+    setTimeout(() => {
+        reject()
+    }, 15000);
+
     // Log messages from the API to the console
+    apiProcess.stdout.setEncoding('utf8');
     apiProcess.stdout.on('data', (data) => {
         // eslint-disable-next-line no-console
         console.log(`API: ${data}`);
+
+        if (data.includes("Application started")) resolve();
+    });
+
+    await promise.catch((e) => {
+        dialog.showMessageBoxSync(this, {
+            type: 'error',
+            buttons: ['OK'],
+            title: 'Confirm',
+            message: 'API was not able to run, the application will be forced quit!'
+        });
+        app.quit()
     });
 }
 
 function createWindow() {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+    });
     mainWindow.setMenuBarVisibility(false);
 
     if (isDevelopment) {
@@ -54,10 +89,14 @@ function createWindow() {
     mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.whenReady().then(() => {
-    startApi();
-    // TODO: Either make sure that we wait until the API is running, or ensure that the frontend can handle the API not being available initially.
+app.whenReady().then(async () => {
+    await startApi();
     createWindow();
+
+    // From Electron docs: macOS apps generally continue running even without any windows open, and activating the app when no windows are available should open a new one.
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
 });
 
 app.on('window-all-closed', () => {
@@ -71,11 +110,3 @@ app.on('before-quit', () => {
     apiProcess = null;
 });
 
-app.on('activate', () => {
-    if (mainWindow == null) {
-        createWindow();
-    }
-    if (apiProcess == null) {
-        startApi();
-    }
-});
