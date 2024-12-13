3917e77b8554316f21557a9409e0c616ff6c29e7
Elias Bruvik <elias.kristoffer.bruvik@gmail.com>
2024-12-13 15:11:20 +0100

    FIX-2627 ignore empty lines when importing csv log data

--- Src/WitsmlExplorer.Frontend/components/Modals/LogDataImportModal.tsx
+++ Src/WitsmlExplorer.Frontend/components/Modals/LogDataImportModal.tsx
@@ -218,7 +218,10 @@ const LogDataImportModal = (
         // csv files
         const headerLine = text.split("\n", 1)[0];
         header = parseCSVHeader(headerLine);
-        data = text.split("\n").slice(1);
+        data = text
+          .split("\n")
+          .slice(1)
+          .filter((row) => row !== "");
         setUploadedFileData(data);
         setAllUploadedFileData(data);
       }
