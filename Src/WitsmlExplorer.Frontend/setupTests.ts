c7957af2eab37fce05298d6c04f0bd7f174b0c0c
Robert Basti <robert.basti@tietoevry.com>
2025-05-23 12:23:14 +0200

    Update setupTests.ts

--- Src/WitsmlExplorer.Frontend/setupTests.ts
+++ Src/WitsmlExplorer.Frontend/setupTests.ts
@@ -1,8 +1,8 @@
 import * as Matchers from "@testing-library/jest-dom/matchers";
 import { expect } from "vitest";
- 
+
 expect.extend(Matchers);
- 
+
 //temporary solution to workaround innapropriate types from @testing-library/jest-dom/vitest out of runtime
 declare module "vitest" {
   /* eslint-disable @typescript-eslint/no-empty-object-type */
