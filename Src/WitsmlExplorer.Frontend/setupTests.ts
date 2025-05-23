f40bf12fa476f7cfc59fc8f43abf5cea3db258f6
Robert Basti <robert.basti@tietoevry.com>
2025-05-23 12:09:49 +0200

    Update setupTests.ts

--- Src/WitsmlExplorer.Frontend/setupTests.ts
+++ Src/WitsmlExplorer.Frontend/setupTests.ts
@@ -1,7 +1,8 @@
-import * as matchers from "@testing-library/jest-dom/matchers";
+import * as Matchers from "@testing-library/jest-dom/matchers";
 import { expect } from "vitest";
-
-expect.extend(matchers);
+ 
+expect.extend(Matchers);
+ 
 //temporary solution to workaround innapropriate types from @testing-library/jest-dom/vitest out of runtime
 declare module "vitest" {
     interface Assertion<T = any> extends Matchers.TestingLibraryMatchers<T, void> {}
