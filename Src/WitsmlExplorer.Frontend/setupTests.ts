0095a15c133700d70e05cb141997b26cef75e150
Robert Basti <robert.basti@tietoevry.com>
2025-05-23 12:14:10 +0200

    Update setupTests.ts

--- Src/WitsmlExplorer.Frontend/setupTests.ts
+++ Src/WitsmlExplorer.Frontend/setupTests.ts
@@ -5,5 +5,6 @@ expect.extend(Matchers);
  
 //temporary solution to workaround innapropriate types from @testing-library/jest-dom/vitest out of runtime
 declare module "vitest" {
+    /* eslint-disable @typescript-eslint/no-empty-object-type */
     interface Assertion<T = any> extends Matchers.TestingLibraryMatchers<T, void> {}
 }
