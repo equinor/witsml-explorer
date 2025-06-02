ec07b27bafbbc8032483068f51ca016aa75b8e44
Robert Basti <robert.basti@tietoevry.com>
2025-06-02 14:44:17 +0200

    fix

--- Src/WitsmlExplorer.Frontend/models/reports/BaseReport.tsx
+++ Src/WitsmlExplorer.Frontend/models/reports/BaseReport.tsx
@@ -2,7 +2,7 @@ export default interface BaseReport {
   title: string;
   summary: string;
   reportItems: any[];
-  dateTimeColumns?: string;
+  dateTimeColumns?: string[];
   warningMessage?: string;
   hasFile?: boolean;
   jobDetails?: string;
@@ -12,7 +12,7 @@ export const createReport = (
   title = "",
   summary = "",
   reportItems: any[] = [],
-  dateTimeColumns: string = null,
+  dateTimeColumns: string[] = null,
   warningMessage: string = null,
   hasFile: boolean = null,
   jobDetails: string = null
