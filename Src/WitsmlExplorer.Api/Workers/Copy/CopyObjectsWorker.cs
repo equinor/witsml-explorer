afc48e3ce99ac933894c32fd57cac6b788d96dcf
Elias Bruvik <elias.kristoffer.bruvik@gmail.com>
2025-06-19 17:37:50 +0200

    FIX-2765 Split queries into chunks to avoid timeout issues

--- Src/WitsmlExplorer.Api/Workers/Copy/CopyObjectsWorker.cs
+++ Src/WitsmlExplorer.Api/Workers/Copy/CopyObjectsWorker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -44,20 +45,31 @@ namespace WitsmlExplorer.Api.Workers.Copy
         private async Task<(WorkerResult, RefreshAction)> GenericCopy(CopyObjectsJob job, CancellationToken? cancellationToken = null)
         {
             var duplicate = job.TargetObjectUid != null;
+            if (duplicate && job.Source.ObjectUids.Length != 1)
+            {
+                throw new ArgumentException("You can only duplicate one object at a time.");
+            }
+
             Witsml.IWitsmlClient targetClient = GetTargetWitsmlClientOrThrow();
             Witsml.IWitsmlClient sourceClient = GetSourceWitsmlClientOrThrow();
-            IWitsmlObjectList fetchObjectsQuery = ObjectQueries.GetWitsmlObjectsByIds(job.Source.WellUid, job.Source.WellboreUid, job.Source.ObjectUids, job.Source.ObjectType);
-            Task<IWitsmlObjectList> fetchObjectsTask = sourceClient.GetFromStoreNullableAsync(fetchObjectsQuery, new OptionsIn(ReturnElements.All));
-            Task<WitsmlWellbore> fetchWellboreTask = WorkerTools.GetWellbore(targetClient, job.Target, retry: true);
-            await Task.WhenAll(fetchObjectsTask, fetchWellboreTask);
-            IWitsmlObjectList objectsToCopy = fetchObjectsTask.Result;
-            WitsmlWellbore targetWellbore = fetchWellboreTask.Result;
 
-            if (objectsToCopy == null)
+            WitsmlWellbore targetWellbore = await WorkerTools.GetWellbore(targetClient, job.Target, retry: true);
+
+            List<IWitsmlObjectList> fetchObjectsQueries = job.Source.ObjectUids.Chunk(10).Select(chunk =>
+                    ObjectQueries.GetWitsmlObjectsByIds(job.Source.WellUid, job.Source.WellboreUid, chunk, job.Source.ObjectType)
+                ).ToList();
+
+            List<Task<IWitsmlObjectList>> fetchObjectsTasks = fetchObjectsQueries.Select(query => sourceClient.GetFromStoreNullableAsync(query, new OptionsIn(ReturnElements.All))).ToList();
+
+            await Task.WhenAll(fetchObjectsTasks);
+
+            List<IWitsmlObjectList> objectListsToCopy = fetchObjectsTasks.Select(task => task.Result).ToList();
+
+            if (objectListsToCopy.Any(witsmlObjectList => witsmlObjectList == null))
             {
                 return (new WorkerResult(targetClient.GetServerHostname(), false, "Failed to deserialize response from Witsml server when fetching objects to copy", sourceServerUrl: sourceClient.GetServerHostname()), null);
             }
-            if (!objectsToCopy.Objects.Any())
+            if (objectListsToCopy.All(witsmlObjectList => !witsmlObjectList.Objects.Any()))
             {
                 return (new WorkerResult(targetClient.GetServerHostname(), false, "Could not find any objects to copy", sourceServerUrl: sourceClient.GetServerHostname()), null);
             }
@@ -66,7 +78,9 @@ namespace WitsmlExplorer.Api.Workers.Copy
                 return (new WorkerResult(targetClient.GetServerHostname(), false, CancellationMessage(), CancellationReason(), sourceServerUrl: sourceClient.GetServerHostname()), null);
             }
 
-            ICollection<WitsmlObjectOnWellbore> queries = ObjectQueries.CopyObjectsQuery(objectsToCopy.Objects, targetWellbore);
+            List<WitsmlObjectOnWellbore> objectsToCopy = objectListsToCopy.SelectMany(objectsToCopy => objectsToCopy.Objects).ToList();
+
+            ICollection<WitsmlObjectOnWellbore> queries = ObjectQueries.CopyObjectsQuery(objectsToCopy, targetWellbore);
             if (duplicate)
             {
                 queries.First().Uid = job.TargetObjectUid;
