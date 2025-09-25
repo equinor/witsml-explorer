55a717aedab06647890d3c53b8d7027461e2ae16
Vaclav Basniar <vaclav.basniar@tietoevry.com>
2025-09-25 14:57:25 +0200

    Revert "- removed randomcolor package"
    This reverts commit af4f5a68de84eb5f412a41bdb3b7e2f77d69b527.

--- Src/WitsmlExplorer.Frontend/components/ContentViews/MultiLogCurveSelection/MultiLogCurveSelectionValues.tsx
+++ Src/WitsmlExplorer.Frontend/components/ContentViews/MultiLogCurveSelection/MultiLogCurveSelectionValues.tsx
@@ -32,6 +32,7 @@ import { toDate } from "date-fns-tz";
 import styled from "styled-components";
 import { MultiLogMetadata } from "../../MultiLogUtils.tsx";
 import { RouterLogType } from "../../../routes/routerConstants.ts";
+import randomColor from "randomcolor";
 import { Colors, dark } from "../../../styles/Colors.tsx";
 import AdjustDepthIndexRange from "../../Modals/TrimLogObject/AdjustDepthIndexRange.tsx";
 import AdjustDateTimeIndexRange from "../../Modals/TrimLogObject/AdjustDateTimeIndexRange.tsx";
@@ -123,7 +124,12 @@ const MultiLogCurveSelectionValues = (
       } as Colors;
 
       colorsCopy.interactive.tableHeaderFillResting =
-        colorPalette[i % colorPalette.length];
+        i <= colorPalette.length
+          ? colorPalette[i]
+          : randomColor({
+              luminosity: "dark",
+              hue: colorPalette[i % colorPalette.length]
+            });
 
       serverColorMap[usedServers[i].id] = colorsCopy;
     }
