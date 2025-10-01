a6e275bff0500f3666d20f0360dfa5f4b7e7728b
Vaclav Basniar <vaclav.basniar@tietoevry.com>
2025-10-01 11:47:16 +0200

    - fixed 500 for UidMappingBasicInfo

--- Src/WitsmlExplorer.Frontend/hooks/query/useGetUidMappingBasicInfos.tsx
+++ Src/WitsmlExplorer.Frontend/hooks/query/useGetUidMappingBasicInfos.tsx
@@ -3,17 +3,22 @@ import { QueryOptions } from "./queryOptions.tsx";
 import UidMappingService from "../../services/uidMappingService.tsx";
 import { UidMappingBasicInfo } from "../../models/uidMapping.tsx";
 import { QueryObserverResult, useQuery } from "@tanstack/react-query";
+import { Server } from "../../models/server.ts";
 
 export const getUidMappingBasicInfosQueryKey = () => {
   return [QUERY_KEY_UID_MAPPING_BASIC_INFOS];
 };
 
-export const uidMappingBasicInfosQuery = (options?: QueryOptions) => ({
+export const uidMappingBasicInfosQuery = (
+  server: Server,
+  options?: QueryOptions
+) => ({
   queryKey: getUidMappingBasicInfosQueryKey(),
   queryFn: async () => {
-    return await UidMappingService.getUidMappingBasicInfos();
+    return await UidMappingService.getUidMappingBasicInfos(server);
   },
-  ...options
+  ...options,
+  enabled: !!server && !(options?.enabled === false)
 });
 
 type UidMappingBasicInfosQueryResult = Omit<
@@ -24,10 +29,11 @@ type UidMappingBasicInfosQueryResult = Omit<
 };
 
 export const useGetUidMappingBasicInfos = (
+  server: Server,
   options?: QueryOptions
 ): UidMappingBasicInfosQueryResult => {
   const { data, ...state } = useQuery<UidMappingBasicInfo[]>(
-    uidMappingBasicInfosQuery(options)
+    uidMappingBasicInfosQuery(server, options)
   );
 
   return { uidMappingBasicInfos: data, ...state };
