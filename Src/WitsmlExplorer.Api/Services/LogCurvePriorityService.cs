7964334c768219aa8c824d772434fad8dfc85b6d
Robert Basti <robert.basti@tietoevry.com>
2025-11-24 15:52:25 +0100

    Update LogCurvePriorityService.cs

--- Src/WitsmlExplorer.Api/Services/LogCurvePriorityService.cs
+++ Src/WitsmlExplorer.Api/Services/LogCurvePriorityService.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using WitsmlExplorer.Api.Extensions;
+using Microsoft.IdentityModel.Tokens;
+
 using WitsmlExplorer.Api.Models;
 using WitsmlExplorer.Api.Repositories;
 
@@ -22,105 +23,105 @@ namespace WitsmlExplorer.Api.Services
         : ILogCurvePriorityService
     {
         private const string UniversalDbId = "universal";
-	    public async Task<IList<string>> GetPrioritizedLocalCurves(string wellUid, string wellboreUid)
-	    {
-	        string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
-	        LogCurvePriority logCurvePriority = await logCurvePriorityRepository.GetDocumentAsync(logCurvePriorityId);
-	        return logCurvePriority?.PrioritizedCurves;
-	    }
+        public async Task<IList<string>> GetPrioritizedLocalCurves(string wellUid, string wellboreUid)
+        {
+            string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
+            LogCurvePriority logCurvePriority = await logCurvePriorityRepository.GetDocumentAsync(logCurvePriorityId);
+            return logCurvePriority?.PrioritizedCurves;
+        }
 
-	    public async Task<IList<string>> SetPrioritizedLocalCurves(string wellUid, string wellboreUid, IList<string> prioritizedCurves)
-	    {
-	        if (prioritizedCurves.IsNullOrEmpty())
-	        {
-	            await DeleteLogCurvePriorityObject(wellUid, wellboreUid);
-	            return null;
-	        }
-	
-	        prioritizedCurves = prioritizedCurves.Distinct().ToList();
-	        string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
-	        LogCurvePriority logCurvePriority = await logCurvePriorityRepository.GetDocumentAsync(logCurvePriorityId);
-	        if (logCurvePriority == null)
-	        {
-	            return await CreatePrioritizedCurves(wellUid, wellboreUid, prioritizedCurves);
-	        }
-	
-	        LogCurvePriority logCurvePriorityToUpdate = CreateLogCurvePriorityLocalObject(wellUid, wellboreUid, prioritizedCurves);
-	        LogCurvePriority updatedLogCurvePriority = await logCurvePriorityRepository.UpdateDocumentAsync(logCurvePriorityId, logCurvePriorityToUpdate);
-	        return updatedLogCurvePriority.PrioritizedCurves;
-	    }
-	
-	    public async Task<IList<string>> GetPrioritizedUniversalCurves()
-	    {
-	        LogCurvePriority logCurvePriorityGlobal = await logCurvePriorityRepository.GetDocumentAsync(UniversalDbId);
-	        return logCurvePriorityGlobal?.PrioritizedCurves;
-	    }
-	
-	    public async Task<IList<string>> SetPrioritizedUniversalCurves(List<string> prioritizedCurves)
-	    {
-	        prioritizedCurves = prioritizedCurves.Distinct().ToList();
-	        var globalDocument = await logCurvePriorityRepository.GetDocumentAsync(UniversalDbId);
-	        if (globalDocument == null)
-	        {
-	            return await CreatePrioritizedUniversalCurves(prioritizedCurves);
-	        }
-	        return await UpdatePrioritizedUniversalCurves(prioritizedCurves);
-	    }
-	
-	
-	    private async Task<IList<string>> CreatePrioritizedCurves(string wellUid, string wellboreUid, IList<string> prioritizedCurves)
-	    {
-	        LogCurvePriority logCurvePriorityToCreate = CreateLogCurvePriorityLocalObject(wellUid, wellboreUid, prioritizedCurves);
-	        LogCurvePriority inserted = await logCurvePriorityRepository.CreateDocumentAsync(logCurvePriorityToCreate);
-	        return inserted.PrioritizedCurves;
-	    }
-	
-	    private async Task DeleteLogCurvePriorityObject(string wellUid, string wellboreUid)
-	    {
-	        IList<string> currentPrioritizedCurves = await GetPrioritizedLocalCurves(wellUid, wellboreUid);
-	        if (currentPrioritizedCurves != null)
-	        {
-	            string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
-	            await logCurvePriorityRepository.DeleteDocumentAsync(logCurvePriorityId);
-	        }
-	    }
-	
-	    private string GetLogCurvePriorityId(string wellUid, string wellboreUid)
-	    {
-	        return $"{wellUid}-{wellboreUid}";
-	    }
-	
-	    private LogCurvePriority CreateLogCurvePriorityLocalObject(string wellUid, string wellboreUid, IList<string> prioritizedCurves)
-	    {
-	        string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
-	        LogCurvePriority logCurvePriorityObject = new(logCurvePriorityId)
-	        {
-	            PrioritizedCurves = prioritizedCurves
-	        };
-	        return logCurvePriorityObject;
-	    }
-	
-	    private async Task<IList<string>> CreatePrioritizedUniversalCurves(List<string> logCurvePriorities)
-	    {
-	        LogCurvePriority logCurvePriorityToCreate = CreateLogCurveUniversalPriorityObject(logCurvePriorities);
-	        LogCurvePriority inserted = await logCurvePriorityRepository.CreateDocumentAsync(logCurvePriorityToCreate);
-	        return inserted.PrioritizedCurves;
-	    }
+        public async Task<IList<string>> SetPrioritizedLocalCurves(string wellUid, string wellboreUid, IList<string> prioritizedCurves)
+        {
+            if (prioritizedCurves.IsNullOrEmpty())
+            {
+                await DeleteLogCurvePriorityObject(wellUid, wellboreUid);
+                return null;
+            }
 
-	    private async Task<IList<string>> UpdatePrioritizedUniversalCurves(List<string> logCurvePriorities)
-	    {
-	        LogCurvePriority logCurvePriorityToCreate = CreateLogCurveUniversalPriorityObject(logCurvePriorities);
-	        LogCurvePriority updated = await logCurvePriorityRepository.UpdateDocumentAsync(UniversalDbId, logCurvePriorityToCreate);
-	        return updated.PrioritizedCurves;
-	    }
-	
-	    private LogCurvePriority CreateLogCurveUniversalPriorityObject(List<string> prioritizedCurves)
-	    {
-	        LogCurvePriority logCurvePriorityObject = new(UniversalDbId)
-	        {
-	            PrioritizedCurves = prioritizedCurves
-	        };
-	        return logCurvePriorityObject;
-	    }
-	}
+            prioritizedCurves = prioritizedCurves.Distinct().ToList();
+            string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
+            LogCurvePriority logCurvePriority = await logCurvePriorityRepository.GetDocumentAsync(logCurvePriorityId);
+            if (logCurvePriority == null)
+            {
+                return await CreatePrioritizedCurves(wellUid, wellboreUid, prioritizedCurves);
+            }
+
+            LogCurvePriority logCurvePriorityToUpdate = CreateLogCurvePriorityLocalObject(wellUid, wellboreUid, prioritizedCurves);
+            LogCurvePriority updatedLogCurvePriority = await logCurvePriorityRepository.UpdateDocumentAsync(logCurvePriorityId, logCurvePriorityToUpdate);
+            return updatedLogCurvePriority.PrioritizedCurves;
+        }
+
+        public async Task<IList<string>> GetPrioritizedUniversalCurves()
+        {
+            LogCurvePriority logCurvePriorityGlobal = await logCurvePriorityRepository.GetDocumentAsync(UniversalDbId);
+            return logCurvePriorityGlobal?.PrioritizedCurves;
+        }
+
+        public async Task<IList<string>> SetPrioritizedUniversalCurves(List<string> prioritizedCurves)
+        {
+            prioritizedCurves = prioritizedCurves.Distinct().ToList();
+            var globalDocument = await logCurvePriorityRepository.GetDocumentAsync(UniversalDbId);
+            if (globalDocument == null)
+            {
+                return await CreatePrioritizedUniversalCurves(prioritizedCurves);
+            }
+            return await UpdatePrioritizedUniversalCurves(prioritizedCurves);
+        }
+
+
+        private async Task<IList<string>> CreatePrioritizedCurves(string wellUid, string wellboreUid, IList<string> prioritizedCurves)
+        {
+            LogCurvePriority logCurvePriorityToCreate = CreateLogCurvePriorityLocalObject(wellUid, wellboreUid, prioritizedCurves);
+            LogCurvePriority inserted = await logCurvePriorityRepository.CreateDocumentAsync(logCurvePriorityToCreate);
+            return inserted.PrioritizedCurves;
+        }
+
+        private async Task DeleteLogCurvePriorityObject(string wellUid, string wellboreUid)
+        {
+            IList<string> currentPrioritizedCurves = await GetPrioritizedLocalCurves(wellUid, wellboreUid);
+            if (currentPrioritizedCurves != null)
+            {
+                string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
+                await logCurvePriorityRepository.DeleteDocumentAsync(logCurvePriorityId);
+            }
+        }
+
+        private string GetLogCurvePriorityId(string wellUid, string wellboreUid)
+        {
+            return $"{wellUid}-{wellboreUid}";
+        }
+
+        private LogCurvePriority CreateLogCurvePriorityLocalObject(string wellUid, string wellboreUid, IList<string> prioritizedCurves)
+        {
+            string logCurvePriorityId = GetLogCurvePriorityId(wellUid, wellboreUid);
+            LogCurvePriority logCurvePriorityObject = new(logCurvePriorityId)
+            {
+                PrioritizedCurves = prioritizedCurves
+            };
+            return logCurvePriorityObject;
+        }
+
+        private async Task<IList<string>> CreatePrioritizedUniversalCurves(List<string> logCurvePriorities)
+        {
+            LogCurvePriority logCurvePriorityToCreate = CreateLogCurveUniversalPriorityObject(logCurvePriorities);
+            LogCurvePriority inserted = await logCurvePriorityRepository.CreateDocumentAsync(logCurvePriorityToCreate);
+            return inserted.PrioritizedCurves;
+        }
+
+        private async Task<IList<string>> UpdatePrioritizedUniversalCurves(List<string> logCurvePriorities)
+        {
+            LogCurvePriority logCurvePriorityToCreate = CreateLogCurveUniversalPriorityObject(logCurvePriorities);
+            LogCurvePriority updated = await logCurvePriorityRepository.UpdateDocumentAsync(UniversalDbId, logCurvePriorityToCreate);
+            return updated.PrioritizedCurves;
+        }
+
+        private LogCurvePriority CreateLogCurveUniversalPriorityObject(List<string> prioritizedCurves)
+        {
+            LogCurvePriority logCurvePriorityObject = new(UniversalDbId)
+            {
+                PrioritizedCurves = prioritizedCurves
+            };
+            return logCurvePriorityObject;
+        }
+    }
 }
