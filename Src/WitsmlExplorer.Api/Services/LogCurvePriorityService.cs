6372004a4d608bd89302ccebe7c9063e82973027
Robert Basti <robert.basti@tietoevry.com>
2025-11-24 15:57:54 +0100

    Update LogCurvePriorityService.cs

--- Src/WitsmlExplorer.Api/Services/LogCurvePriorityService.cs
+++ Src/WitsmlExplorer.Api/Services/LogCurvePriorityService.cs
@@ -2,8 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Microsoft.IdentityModel.Tokens;
-
+using WitsmlExplorer.Api.Extensions;
 using WitsmlExplorer.Api.Models;
 using WitsmlExplorer.Api.Repositories;
 
