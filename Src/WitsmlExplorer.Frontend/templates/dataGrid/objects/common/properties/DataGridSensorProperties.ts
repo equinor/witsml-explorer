a2319f04eb0acea3223805c20b300fdc1c7b2b73
Elias Bruvik <elias.kristoffer.bruvik@gmail.com>
2026-03-10 15:57:20 +0100

    Update tubular

--- Src/WitsmlExplorer.Frontend/templates/dataGrid/objects/common/properties/DataGridSensorProperties.ts
+++ Src/WitsmlExplorer.Frontend/templates/dataGrid/objects/common/properties/DataGridSensorProperties.ts
@@ -7,20 +7,35 @@ import { dataGridUomProperties } from "templates/dataGrid/objects/common/propert
 export const dataGridSensorProperties: DataGridProperty[] = [
   {
     name: "uid",
+    required: false,
+    baseType: "string",
+    witsmlType: "uidString",
+    maxLength: 64,
     documentation: "Unique identifier for the sensor.",
     isAttribute: true
   },
   {
     name: "typeMeasurement",
+    required: false,
+    baseType: "string",
+    witsmlType: "measurementType",
+    maxLength: 50,
     documentation: "Type from POSC."
   },
   {
     name: "offsetBot",
+    required: false,
+    baseType: "double",
+    witsmlType: "lengthMeasure",
     documentation: "Offset from bottom of measurement while drilling tool.",
-    properties: dataGridUomProperties()
+    properties: dataGridUomProperties("lengthUom")
   },
   {
     name: "comments",
+    required: false,
+    baseType: "string",
+    witsmlType: "commentString",
+    maxLength: 4000,
     documentation: "Comments and remarks."
   },
   dataGridCustomData,
