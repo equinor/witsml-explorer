f667174138b6c0943b3030b2ed264c0effa445d7
Elias Bruvik <elias.kristoffer.bruvik@gmail.com>
2026-05-22 14:43:22 +0200

    FIX-3006 Add EtpAttachmentService

--- Src/WitsmlExplorer.Api/HttpHandlers/AttachmentHandler.cs
+++ Src/WitsmlExplorer.Api/HttpHandlers/AttachmentHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
@@ -6,21 +7,30 @@ using Microsoft.AspNetCore.Mvc;
 
 using WitsmlExplorer.Api.Models;
 using WitsmlExplorer.Api.Services;
+using WitsmlExplorer.Api.Services.ETP;
 
 namespace WitsmlExplorer.Api.HttpHandlers
 {
     public static class AttachmentHandler
     {
         [Produces(typeof(IEnumerable<Attachment>))]
-        public static async Task<IResult> GetAttachments(string wellUid, string wellboreUid, IAttachmentService attachmentService)
+        public static async Task<IResult> GetAttachments(HttpContext httpContext, string wellUid, string wellboreUid, IAttachmentService attachmentService, IEtpAttachmentService etpAttachmentService, IProtocolCoordinator protocolCoordinator, CancellationToken cancellationToken)
         {
-            return TypedResults.Ok(await attachmentService.GetAttachments(wellUid, wellboreUid));
-
+            EssentialHeaders eh = new(httpContext?.Request);
+            var protocol = (eh.WitsmlProtocol == WitsmlProtocol.Etp && !string.IsNullOrWhiteSpace(wellUid) && !string.IsNullOrWhiteSpace(wellboreUid)) ? WitsmlProtocol.Etp : WitsmlProtocol.Soap;
+            Task<ICollection<Attachment>> SoapCall() => attachmentService.GetAttachments(wellUid, wellboreUid);
+            Task<ICollection<Attachment>> EtpCall() => etpAttachmentService.GetAttachments(wellUid, wellboreUid, cancellationToken);
+            return await protocolCoordinator.ExecuteOkAsync(httpContext, protocol, SoapCall, EtpCall);
         }
+
         [Produces(typeof(Attachment))]
-        public static async Task<IResult> GetAttachment(string wellUid, string wellboreUid, string attachmentUid, IAttachmentService attachmentService)
+        public static async Task<IResult> GetAttachment(HttpContext httpContext, string wellUid, string wellboreUid, string attachmentUid, IAttachmentService attachmentService, IEtpAttachmentService etpAttachmentService, IProtocolCoordinator protocolCoordinator, CancellationToken cancellationToken)
         {
-            return TypedResults.Ok(await attachmentService.GetAttachment(wellUid, wellboreUid, attachmentUid));
+            EssentialHeaders eh = new(httpContext?.Request);
+            var protocol = (eh.WitsmlProtocol == WitsmlProtocol.Etp && !string.IsNullOrWhiteSpace(wellUid) && !string.IsNullOrWhiteSpace(wellboreUid)) ? WitsmlProtocol.Etp : WitsmlProtocol.Soap;
+            Task<Attachment> SoapCall() => attachmentService.GetAttachment(wellUid, wellboreUid, attachmentUid);
+            Task<Attachment> EtpCall() => etpAttachmentService.GetAttachment(wellUid, wellboreUid, attachmentUid, cancellationToken);
+            return await protocolCoordinator.ExecuteOkAsync(httpContext, protocol, SoapCall, EtpCall);
         }
     }
 }
---
+++ Src/WitsmlExplorer.Api/Services/ETP/EtpAttachmentService.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Energistics.Datatypes.Object;
+
+using Witsml.Data;
+
+using WitsmlExplorer.Api.Models;
+
+namespace WitsmlExplorer.Api.Services.ETP
+{
+    public interface IEtpAttachmentService
+    {
+        Task<Attachment> GetAttachment(string wellUid, string wellboreUid, string attachmentUid, CancellationToken? cancellationToken);
+        Task<ICollection<Attachment>> GetAttachments(string wellUid, string wellboreUid, CancellationToken? cancellationToken);
+    }
+
+    public class EtpAttachmentService : EtpService, IEtpAttachmentService
+    {
+        public EtpAttachmentService(IEtpClientProvider etpClientProvider) : base(etpClientProvider)
+        {
+        }
+
+        public async Task<Attachment> GetAttachment(string wellUid, string wellboreUid, string attachmentUid, CancellationToken? cancellationToken)
+        {
+            var client = await GetEtpClient(cancellationToken);
+            var uri = EtpUriHelper.CreateObjectUri(wellUid, wellboreUid, EntityType.Attachment, attachmentUid);
+            var objList = await client.GetObjectAsWitsmlAsync<WitsmlAttachments>(uri, cancellationToken ?? CancellationToken.None);
+            if (objList == null || !objList.Objects.Any())
+            {
+                return null;
+            }
+
+            return Attachment.FromWitsml(objList.Attachments.FirstOrDefault());
+        }
+
+        public async Task<ICollection<Attachment>> GetAttachments(string wellUid, string wellboreUid, CancellationToken? cancellationToken)
+        {
+            var client = await GetEtpClient(cancellationToken);
+            var uri = EtpUriHelper.CreateObjectUri(wellUid, wellboreUid, EntityType.Attachment);
+            var resources = await client.GetResourcesAsync(uri, cancellationToken ?? CancellationToken.None);
+            var attachments = resources.Select(MapResourceToAttachment).ToList();
+
+            return attachments;
+        }
+
+        private Attachment MapResourceToAttachment(Resource resource)
+        {
+            return new Attachment
+            {
+                Uid = EtpUriHelper.GetObjectUid(resource.uri, EntityType.Attachment),
+                Name = resource.name,
+                WellboreUid = EtpUriHelper.GetWellboreUid(resource.uri),
+                WellUid = EtpUriHelper.GetWellUid(resource.uri)
+            };
+        }
+    }
+}
