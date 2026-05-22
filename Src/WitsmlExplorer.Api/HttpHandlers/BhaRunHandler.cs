11a1202d587ebf31cfcfb26dad838b6638385510
Elias Bruvik <elias.kristoffer.bruvik@gmail.com>
2026-05-22 13:57:13 +0200

    FIX-3006 Add EtpBhaRunService

--- Src/WitsmlExplorer.Api/HttpHandlers/BhaRunHandler.cs
+++ Src/WitsmlExplorer.Api/HttpHandlers/BhaRunHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
@@ -6,21 +7,29 @@ using Microsoft.AspNetCore.Mvc;
 
 using WitsmlExplorer.Api.Models;
 using WitsmlExplorer.Api.Services;
+using WitsmlExplorer.Api.Services.ETP;
 
 namespace WitsmlExplorer.Api.HttpHandlers
 {
     public static class BhaRunHandler
     {
         [Produces(typeof(IEnumerable<BhaRun>))]
-        public static async Task<IResult> GetBhaRuns(string wellUid, string wellboreUid, IBhaRunService bhaRunService)
+        public static async Task<IResult> GetBhaRuns(HttpContext httpContext, string wellUid, string wellboreUid, IBhaRunService bhaRunService, IEtpBhaRunService etpBhaRunService, IProtocolCoordinator protocolCoordinator, CancellationToken cancellationToken)
         {
-            return TypedResults.Ok(await bhaRunService.GetBhaRuns(wellUid, wellboreUid));
-
+            EssentialHeaders eh = new(httpContext?.Request);
+            var protocol = (eh.WitsmlProtocol == WitsmlProtocol.Etp && !string.IsNullOrWhiteSpace(wellUid) && !string.IsNullOrWhiteSpace(wellboreUid)) ? WitsmlProtocol.Etp : WitsmlProtocol.Soap;
+            Task<ICollection<BhaRun>> SoapCall() => bhaRunService.GetBhaRuns(wellUid, wellboreUid);
+            Task<ICollection<BhaRun>> EtpCall() => etpBhaRunService.GetBhaRuns(wellUid, wellboreUid, cancellationToken);
+            return await protocolCoordinator.ExecuteOkAsync(httpContext, protocol, SoapCall, EtpCall);
         }
         [Produces(typeof(BhaRun))]
-        public static async Task<IResult> GetBhaRun(string wellUid, string wellboreUid, string bhaRunUid, IBhaRunService bhaRunService)
+        public static async Task<IResult> GetBhaRun(HttpContext httpContext, string wellUid, string wellboreUid, string bhaRunUid, IBhaRunService bhaRunService, IEtpBhaRunService etpBhaRunService, IProtocolCoordinator protocolCoordinator, CancellationToken cancellationToken)
         {
-            return TypedResults.Ok(await bhaRunService.GetBhaRun(wellUid, wellboreUid, bhaRunUid));
+            EssentialHeaders eh = new(httpContext?.Request);
+            var protocol = (eh.WitsmlProtocol == WitsmlProtocol.Etp && !string.IsNullOrWhiteSpace(wellUid) && !string.IsNullOrWhiteSpace(wellboreUid)) ? WitsmlProtocol.Etp : WitsmlProtocol.Soap;
+            Task<BhaRun> SoapCall() => bhaRunService.GetBhaRun(wellUid, wellboreUid, bhaRunUid);
+            Task<BhaRun> EtpCall() => etpBhaRunService.GetBhaRun(wellUid, wellboreUid, bhaRunUid, cancellationToken);
+            return await protocolCoordinator.ExecuteOkAsync(httpContext, protocol, SoapCall, EtpCall);
         }
     }
 }
---
+++ Src/WitsmlExplorer.Api/Services/ETP/EtpBhaRunService.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Energistics.Datatypes.Object;
+
+using Witsml.Data;
+
+using WitsmlExplorer.Api.Models;
+
+namespace WitsmlExplorer.Api.Services.ETP
+{
+    public interface IEtpBhaRunService
+    {
+        public Task<BhaRun> GetBhaRun(string wellUid, string wellboreUid, string bhaRunUid, CancellationToken? cancellationToken);
+        public Task<ICollection<BhaRun>> GetBhaRuns(string wellUid, string wellboreUid, CancellationToken? cancellationToken);
+    }
+
+    public class EtpBhaRunService : EtpService, IEtpBhaRunService
+    {
+        public EtpBhaRunService(IEtpClientProvider etpClientProvider) : base(etpClientProvider)
+        {
+        }
+
+        public async Task<BhaRun> GetBhaRun(string wellUid, string wellboreUid, string bhaRunUid, CancellationToken? cancellationToken)
+        {
+            var client = await GetEtpClient(cancellationToken);
+            var uri = EtpUriHelper.CreateObjectUri(wellUid, wellboreUid, EntityType.BhaRun, bhaRunUid);
+            var objList = await client.GetObjectAsWitsmlAsync<WitsmlBhaRuns>(uri, cancellationToken ?? CancellationToken.None);
+            if (objList == null || !objList.Objects.Any())
+            {
+                return null;
+            }
+            var bhaRun = BhaRun.FromWitsml(objList.BhaRuns.FirstOrDefault());
+
+            return bhaRun;
+        }
+
+        public async Task<ICollection<BhaRun>> GetBhaRuns(string wellUid, string wellboreUid, CancellationToken? cancellationToken)
+        {
+            var client = await GetEtpClient(cancellationToken);
+            var uri = EtpUriHelper.CreateObjectUri(wellUid, wellboreUid, EntityType.BhaRun);
+            var resources = await client.GetResourcesAsync(uri, cancellationToken ?? CancellationToken.None);
+            var bhaRuns = resources.Select(MapResourceToBhaRun).ToList();
+
+            return bhaRuns;
+        }
+
+        private BhaRun MapResourceToBhaRun(Resource resource)
+        {
+            return new BhaRun
+            {
+                Uid = EtpUriHelper.GetObjectUid(resource.uri, EntityType.BhaRun),
+                Name = resource.name,
+                WellboreUid = EtpUriHelper.GetWellboreUid(resource.uri),
+                WellUid = EtpUriHelper.GetWellUid(resource.uri)
+            };
+        }
+    }
+}
